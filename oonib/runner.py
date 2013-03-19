b12a334845a7727caf696b08f7d63e2c1a19f0a3
aagbsn <aagbsn@extc.org>
2013-03-19 15:20:06 +0000

    Set a default CircuitBuildTimeout. See #6304

--- oonib/runner.py
+++ oonib/runner.py
@@ -43,6 +43,7 @@ def startTor():
     torconfig.SocksPort = 9055
     if config.main.tor2webmode:
         torconfig.Tor2webMode = 1
+        torconfig.CircuitBuildTimeout = 60
     torconfig.save()
     d = txtorcon.launch_tor(torconfig, reactor,
             tor_binary=config.main.tor_binary,
