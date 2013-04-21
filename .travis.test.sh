99d86cfb7a4944e57d179709168e5c7c7e9a64a3
Isis Lovecruft <isis@torproject.org>
2013-04-21 07:25:37 +0000

    Add script for killing the TravisCI spawned oonib after thirty seconds.
     * ∃!isisⒶwintermute:~/code/torproject/ooni-backend ∴ date
       Sun Apr 21 07:25:24 UTC 2013
       ∃!isisⒶwintermute:~/code/torproject/ooni-backend ∴ travis-lint
       Hooray, /home/isis/code/torproject/ooni-backend/.travis.yml seems to be solid!

---
+++ .travis.test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+##############################################################################
+#
+# .travis.test.sh
+# -------------------
+# Run $2 for $1 seconds and then kill the process.
+#
+# :authors: Isis Agora Lovecruft, 0x2cdb8b35
+# :date: 21 April 2013
+# :version: 0.0.1
+##############################################################################
+
+function killitwithfire () {
+    trap - ALRM
+    kill -ALRM $prog 2>/dev/null
+    kill $! 2>/dev/null && exit 124
+}
+
+function waitforit () {
+    trap "killitwithfire" ALRM
+    sleep $1 & wait
+    kill -ALRM $$
+}
+
+waitforit $1& prog=$! ; shift ;
+trap "killitwithfire" ALRM INT
+"$@" & wait $1
+RET=$?
+kill -ALRM $prog
+wait $prog
+exit $RET
