1e0d3c0d0b20f92fd901163a4f2b41627f9e931e
Arturo Filastò <art@fuffa.org>
2013-08-19 14:03:42 +0200

    Add support for serializing lists to json via self.write()

--- oonib/handlers.py
+++ oonib/handlers.py
@@ -1,7 +1,20 @@
+import types
+
+from cyclone import escape
 from cyclone import web
 
 class OONIBHandler(web.RequestHandler):
-    pass
+    def write(self, chunk):
+        """
+        This is a monkey patch to RequestHandler to allow us to serialize also
+        json list objects.
+        """
+        if isinstance(chunk, types.ListType):
+            chunk = escape.json_encode(chunk)
+            web.RequestHandler.write(self, chunk)
+            self.set_header("Content-Type", "application/json")
+        else:
+            web.RequestHandler.write(self, chunk)
 
 class OONIBError(web.HTTPError):
     pass
