639e23d7341c4a12a3cf5b901d1479e2f4fee38b
Arturo Filastò <art@fuffa.org>
2013-09-10 19:52:33 +0200

    Parametrise the DNS test helper remote resolver.

--- oonib/testhelpers/dns_helpers.py
+++ oonib/testhelpers/dns_helpers.py
@@ -3,11 +3,20 @@ from twisted.internet import reactor
 from twisted.names import dns
 from twisted.names import client, server
 
+from oonib import config
+
 class DNSTestHelper(server.DNSServerFactory):
     def __init__(self, authorities = None,
                  caches = None, clients = None,
                  verbose = 0):
-        resolver = client.Resolver(servers=[('8.8.8.8', 53)])
+        try:
+            host, port = config.helpers.dns.split(':')
+            port = int(port)
+        # XXX remove this when we have configuration file versioning.
+        # https://github.com/TheTorProject/ooni-probe/issues/190
+        except:
+            host, port = '8.8.8.8', 53
+        resolver = client.Resolver(servers=[(host, port)])
         server.DNSServerFactory.__init__(self, authorities = authorities,
                                          caches = caches, clients = [resolver],
                                          verbose = verbose)
