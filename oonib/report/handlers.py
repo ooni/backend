eae427c12a5c1ca1ec42c9c10893710eb9ed9d08
Darius Bacon <darius@wry.me>
2014-04-16 12:21:02 -0700

    remove overbroad try/except

--- oonib/report/handlers.py
+++ oonib/report/handlers.py
@@ -250,10 +250,7 @@ class NewReportHandlerFile(OONIBHandler, UpdateReportMixin):
         software_version = str(report_data['software_version'])
         
         probe_asn = str(report_data['probe_asn'])
-        try:
-            probe_cc = str(report_data['probe_cc'])
-        except:
-            probe_cc = 'ZZ'
+        probe_cc = str(report_data.get('probe_cc', 'ZZ'))
 
         self.testName = str(report_data['test_name'])
         self.testVersion = str(report_data['test_version'])
