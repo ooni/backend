8df70710a485365a2657285b4c398d33d69860e2
Arturo Filastò <art@fuffa.org>
2015-05-13 19:35:36 +0200

    Update project.clj to use the hortonworks repo
    Thanks to @dan-blanchard for the swift reply and fix

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :target-path "_build"
   :min-lein-version "2.0.0"
   :jvm-opts ["-client"]
-  :dependencies  [[org.apache.storm/storm-core "0.9.3"]
+  :repositories {"HDP Releases" "http://repo.hortonworks.com/content/repositories/releases"}
+  :dependencies  [[org.apache.storm/storm-core "0.9.3.2.2.4.2-2"]
                   [com.parsely/streamparse "0.0.4-SNAPSHOT"]
                   ]
   :jar-exclusions     [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
