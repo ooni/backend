762956bd4caee721359922d6a27ba421c426fb5a
Arturo Filastò <art@fuffa.org>
2015-05-18 13:46:08 +0200

    Place files into subdirectories

--- hadoop-put.sh
+++ hadoop-put.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 REPORTS_BY_DATE_DIR=/data1/reports/
-for report_file in `find $REPORTS_BY_DATE_DIR`;do
-  hadoop fs -put $report_file /user/ooni/reports/
+for SRC_PATH in $( find $REPORTS_BY_DATE_DIR -name "*.sanitised" );do
+  DST_PATH="/user/ooni/reports/sanitised/$(basename $SRC_PATH | sed s/\.sanitised//)"
+  echo "Copying $SRC_PATH to hdfs://$DST_PATH"
+  hadoop fs -put -f $SRC_PATH $DST_PATH
+done
+for SRC_PATH in $( find $REPORTS_BY_DATE_DIR -name "*.raw" );do
+  DST_PATH="/user/ooni/reports/raw/$(basename $SRC_PATH | sed s/\.raw//)"
+  echo "Copying $SRC_PATH to hdfs://$DST_PATH"
+  hadoop fs -put -f $SRC_PATH $DST_PATH
 done
