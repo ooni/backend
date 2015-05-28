d10ffc35683ef8a9ab37984a1948965ffbd65555
Arturo Filastò <art@fuffa.org>
2015-05-29 00:37:20 +0200

    Fix entries at the root of the problem

--- pipeline/helpers/report.py
+++ pipeline/helpers/report.py
@@ -1,17 +1,103 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import os
+import zlib
 import time
 import random
 import string
 import logging
 import traceback
+from base64 import b64encode
 from datetime import datetime
 
 import yaml
 from pipeline.helpers import sanitise
 
 logger = logging.getLogger('ooni-pipeline')
+
+mappings = {
+    "http_host": ["http_template", "http_host"],
+    "HTTP Host": ["http_template", "http_host"],
+
+    "http_requests_test": ["http_template",
+                           "http_requests"],
+    "http_requests": ["http_template", "http_requests"],
+    "HTTP Requests Test": ["http_template",
+                           "http_requests"],
+
+    "bridge_reachability": ["bridge_reachability"],
+    "bridgereachability": ["bridge_reachability"],
+
+    "TCP Connect": ["tcp_connect"],
+    "tcp_connect": ["tcp_connect"],
+
+    "DNS tamper": ["dns_template", "dns_consistency"],
+    "dns_consistency": ["dns_template", "dns_consistency"],
+
+    "HTTP Invalid Request Line": ["tcp_connect", "http_invalid_request_line"],
+    "http_invalid_request_line": ["tcp_connect", "http_invalid_request_line"],
+
+    "http_header_field_manipulation": ["http_header_field_manipulation"],
+    "HTTP Header Field Manipulation": ["http_header_field_manipulation"],
+
+    "Multi Protocol Traceroute Test": ["scapy_template", "multi_protocol_traceroute"],
+    "multi_protocol_traceroute_test": ["scapy_template", "multi_protocol_traceroute"],
+    "traceroute": ["scapy_template", "multi_protocol_traceroute"],
+
+    "parasitic_traceroute_test": ["scapy_template"],
+
+    "tls-handshake": ["tls_handshake"],
+
+    "dns_injection": ["scapy_template", "dns_injection"],
+
+    "captivep": ["captive_portal"],
+    "captiveportal": ["captive_portal"],
+
+    # These are ignored as we don't yet have analytics for them
+    "HTTPFilteringBypass": False,
+    "HTTPTrix": False,
+    "http_test": False,
+    "http_url_list": False,
+    "dns_spoof": False,
+    "netalyzrwrapper": False,
+
+    # These are ignored because not code for them is available
+    "tor_http_requests_test": False,
+    "sip_requests_test": False,
+    "tor_exit_ip_test": False,
+    "website_probe": False,
+    "base_tcp_test": False,
+
+    # These are ignored because they are invalid reports
+    "summary": False,
+    "test_get": False,
+    "test_put": False,
+    "test_post": False,
+    "this_test_is_nameless": False,
+    "test_send_host_header": False,
+    "test_random_big_request_method": False,
+    "test_get_random_capitalization": False,
+    "test_put_random_capitalization": False,
+    "test_post_random_capitalization": False,
+    "test_random_invalid_field_count": False,
+    "keyword_filtering_detection_based_on_rst_packets": False
+}
+
+def fix_http_requests(entry):
+    requests = []
+    for request in entry["requests"]:
+        try:
+            request["response"]["body"] = unicode(request["response"]["body"])
+        except UnicodeDecodeError:
+            request["response"]["body"] = {
+                "encoding": "base64.gzip",
+                "data": b64encode(zlib.compress(request["response"]["body"])),
+                "doc": "b64encode(zlib.compress(data))"
+            }
+        requests.append(request)
+    entry["requests"] = requests
+    return requests
+
 
 class Report(object):
     def __init__(self, in_file, bridge_db, path):
@@ -64,6 +150,16 @@ class Report(object):
             return entry
         return sanitise.run(self._raw_header['test_name'], entry, self.bridge_db)
 
+    def fix_entry(self, entry):
+        entry["record_type"] = "entry"
+        if not self._sanitised_header.get('test_name'):
+            logger.error("test_name is missing in %s" % entry["report_id"])
+            return entry
+        test_name = self._sanitised_header.get('test_name')
+        if "http_requests" in mappings.get(test_name):
+            entry = fix_http_requests(entry)
+        return entry
+
     def process_entry(self, entry):
         raw_entry = entry.copy()
         sanitised_entry = entry.copy()
@@ -71,8 +167,8 @@ class Report(object):
         raw_entry.update(self._raw_header)
         sanitised_entry.update(self._sanitised_header)
 
-        raw_entry["record_type"] = "entry"
-        sanitised_entry["record_type"] = "entry"
+        raw_entry = self.fix_entry(raw_entry)
+        sanitised_entry = self.fix_entry(sanitised_entry)
 
         sanitised_entry = self.sanitise_entry(sanitised_entry)
         return sanitised_entry, raw_entry
