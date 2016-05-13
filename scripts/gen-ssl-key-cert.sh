d7f9a4c731acd9979376fa73438d4e8afae75446
Arturo Filastò <arturo@filasto.net>
2016-05-13 12:57:38 +0200

    Fix typo in gen-ssl-key-cert script

--- scripts/gen-ssl-key-cert.sh
+++ scripts/gen-ssl-key-cert.sh
@@ -10,4 +10,4 @@ openssl req -x509 -newkey rsa:2048 \
     -keyout private/ssl-key.pem -out private/ssl-cert.pem \
     -days 400 -nodes -subj '/CN=selfie'
 
-cat private/ssl-key.pem private/ssh-cert.pem > private/ssl-key-and-cert.pem
+cat private/ssl-key.pem private/ssl-cert.pem > private/ssl-key-and-cert.pem
