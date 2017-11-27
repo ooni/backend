f3fb9fd75aae557f7e0ad4b5521a4020a39a2fcb
Arturo Filastò <arturo@filasto.net>
2017-11-27 14:39:10 +0100

    Add better handling of 500 type errors

--- measurements/views.py
+++ measurements/views.py
@@ -4,19 +4,41 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import os
+import json
 import pathlib
 
-import connexion
+import werkzeug
+from flask import Response, current_app
+
+from connexion import ProblemException, FlaskApi, Resolver, problem
 
 from measurements.api import api_private_blueprint
 from measurements.pages import pages_blueprint, api_docs_blueprint
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
+def render_problem_exception(exception):
+    response = exception.to_problem()
+    return FlaskApi.get_response(response)
+
+def render_generic_exception(exception):
+    if not isinstance(exception, werkzeug.exceptions.HTTPException):
+        exc_name = "{}.{}".format(type(exception).__module__,
+                                  type(exception).__name__)
+        exc_desc = str(exception)
+        current_app.logger.warning('Unhandled error occurred, {}: {}'.format(exc_name, exc_desc))
+        exception = werkzeug.exceptions.InternalServerError(
+            description='An unhandled application error occurred: {}'.format(exc_name)
+        )
+
+    response = problem(title=exception.name, detail=exception.description,
+                       status=exception.code)
+    return FlaskApi.get_response(response)
+
 def register(app):
     from measurements import api
-    connexion_resolver = connexion.resolver.Resolver()
-    connexion_api = connexion.Api(
+    connexion_resolver = Resolver()
+    connexion_api = FlaskApi(
         specification=pathlib.Path(os.path.join(HERE, 'openapi/measurements.yml')),
         resolver=connexion_resolver,
         arguments=dict(),
@@ -36,3 +58,6 @@ def register(app):
     app.register_blueprint(connexion_api.blueprint)
     app.register_blueprint(api_private_blueprint, url_prefix='/api/_')
     app.register_blueprint(pages_blueprint, url_prefix='')
+
+    app.register_error_handler(ProblemException, render_problem_exception)
+    app.register_error_handler(Exception, render_generic_exception)
