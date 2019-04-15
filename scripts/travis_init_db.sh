47c6f1e57213c4b3903fdbc7b1fb13aa20b5d2bb
Arturo Filastò <arturo@filasto.net>
2019-04-15 19:17:41 +0200

    Maybe fix travis

---
+++ scripts/travis_init_db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+tmpdir=$(mktemp)
+cd $tmpdir
+git clone --depth 1 --branch ooexplr-meta https://github.com/ooni/pipeline.git
+psql -c 'create database ooni_measurements;' -U postgres
+cat af/oometa/*.install.sql | psql -U postgres ooni_measurements
