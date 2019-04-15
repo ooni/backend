4a837e10deaac9f0752b7ea577fdd686d814943e
Arturo Filastò <arturo@filasto.net>
2019-04-15 12:25:22 +0200

    Fix rollback script

--- af/oometa/014-ooexpl-insert.rollback.sql
+++ af/oometa/014-ooexpl-insert.rollback.sql
@@ -2,6 +2,6 @@ BEGIN;
 
 select _v.unregister_patch( '014-ooexpl-insert');
 
-TRUNCATE ooexpl_recent_msm_count, ooexpl_daily_msm_count;
+TRUNCATE ooexpl_bucket_msm_count, ooexpl_daily_msm_count;
 
 COMMIT;
