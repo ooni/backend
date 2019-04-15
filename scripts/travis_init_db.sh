9b56e56284d0709c550fa78ce36b89bb499291a8
Arturo Filastò <arturo@filasto.net>
2019-04-15 19:46:00 +0200

    Fix path

--- scripts/travis_init_db.sh
+++ scripts/travis_init_db.sh
@@ -4,4 +4,4 @@ tmpdir=$(mktemp -d)
 cd $tmpdir
 git clone --depth 1 --branch ooexplr-meta https://github.com/ooni/pipeline.git
 psql -c 'create database ooni_measurements;' -U postgres
-cat af/oometa/*.install.sql | psql -U postgres ooni_measurements
+cat pipeline/af/oometa/*.install.sql | psql -U postgres ooni_measurements
