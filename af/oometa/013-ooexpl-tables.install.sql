de69d52e4133c72b1cd8fca96e339ac19f400f61
Arturo Filastò <arturo@filasto.net>
2019-04-15 12:21:33 +0200

    Save some bytes

--- af/oometa/013-ooexpl-tables.install.sql
+++ af/oometa/013-ooexpl-tables.install.sql
@@ -3,7 +3,7 @@ BEGIN;
 select _v.register_patch( '013-ooexpl-tables', ARRAY[ '012-sha256-input-uniq' ], NULL );
 
 CREATE TABLE ooexpl_bucket_msm_count (
-    "count" bigint,
+    "count" integer,
     "probe_asn" integer,
     "probe_cc" character(2),
     "bucket_date" date,
@@ -13,11 +13,11 @@ CREATE TABLE ooexpl_bucket_msm_count (
 comment on table ooexpl_bucket_msm_count is 'OONI Explorer stats table for counting the total number of measurements since the beginning of time by probe_cc and probe_asn';
 
 CREATE TABLE ooexpl_daily_msm_count (
-    "count" bigint,
+    "count" integer,
     "probe_cc" character(2),
     "probe_asn" integer,
     "test_name" ootest,
-    "test_day" timestamp without time zone,
+    "test_day" date,
     "bucket_date" date,
     CONSTRAINT ooexpl_daily_msm_count_pkey PRIMARY KEY (probe_asn, probe_cc, test_name, test_day, bucket_date)
 );
