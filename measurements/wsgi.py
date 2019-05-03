e2cedc16ee6ee6a707e278d496088ef772cc35b5
Arturo Filastò <arturo@filasto.net>
2019-05-03 11:50:28 +0200

    Add support for exposing API metrics via prometheus

--- measurements/wsgi.py
+++ measurements/wsgi.py
@@ -1,6 +1,12 @@
+# We assume this is going to be running on gunicorn with the gunicorn config
+# via gunicorn --config python:measurement.gunicorn_config
 from werkzeug.contrib.fixers import ProxyFix
 
 from measurements.app import create_app
 
-application = create_app()
-application.wsgi_app = ProxyFix(application.wsgi_app)
+from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
+
+app = create_app()
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+metrics = GunicornPrometheusMetrics(app)
