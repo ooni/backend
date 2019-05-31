0b39ccc283df711091b2354daa1cd62c9019353b
Arturo Filastò <arturo@filasto.net>
2019-05-31 15:05:34 +0200

    Drop query from comment

--- af/shovel/centrifugation.py
+++ af/shovel/centrifugation.py
@@ -1685,32 +1685,7 @@ ON CONFLICT (probe_cc, probe_asn, test_name, test_day, bucket_date) DO
 UPDATE
 SET count = EXCLUDED.count;''', [bucket])
 
-    # Materialized view query:
-    """
-CREATE MATERIALIZED VIEW ooexpl_wc_confirmed AS
-SELECT
-COALESCE(SUM(CASE WHEN confirmed = TRUE THEN 1 ELSE 0 END), 0) as confirmed_count,
-COUNT(*) as msm_count,
-date_trunc('day', test_start_time) as test_day,
-probe_cc,
-probe_asn
-FROM (
-	SELECT
-	DISTINCT input as input,
-	test_start_time,
-	probe_cc,
-	probe_asn,
-	bool_or(confirmed) as confirmed
-	FROM measurement
-	JOIN input ON input.input_no = measurement.input_no
-	JOIN report ON report.report_no = measurement.report_no
-	WHERE test_start_time >= current_date - interval '2 months'
-	AND test_start_time < current_date - interval '1 day'
-	AND test_name = 'web_connectivity'
-	GROUP BY 1,2,3,4
-) as wc
-GROUP BY 3,4,5;
-        """
+        # See:  016-ooexpl_wc_confirmed.install.sql
         c.execute('REFRESH MATERIALIZED VIEW ooexpl_wc_confirmed;')
 
 def meta_pg(in_root, bucket, postgres):
