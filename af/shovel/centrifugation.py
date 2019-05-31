e73ddd340b655fc079794991deca6e68ecd1695e
Arturo Filastò <arturo@filasto.net>
2019-05-31 14:58:38 +0200

    Add task for refreshing the ooni-explorer web_connectivity mat view

--- af/shovel/centrifugation.py
+++ af/shovel/centrifugation.py
@@ -1685,6 +1685,34 @@ ON CONFLICT (probe_cc, probe_asn, test_name, test_day, bucket_date) DO
 UPDATE
 SET count = EXCLUDED.count;''', [bucket])
 
+    # Materialized view query:
+    """
+CREATE MATERIALIZED VIEW ooexpl_wc_confirmed AS
+SELECT
+COALESCE(SUM(CASE WHEN confirmed = TRUE THEN 1 ELSE 0 END), 0),
+COUNT(*),
+date_trunc('day', test_start_time),
+probe_cc,
+probe_asn
+FROM (
+	SELECT
+	DISTINCT input as input,
+	test_start_time,
+	probe_cc,
+	probe_asn,
+	bool_or(confirmed) as confirmed
+	FROM measurement
+	JOIN input ON input.input_no = measurement.input_no
+	JOIN report ON report.report_no = measurement.report_no
+	WHERE test_start_time >= current_date - interval '2 months'
+	AND test_start_time < current_date - interval '1 day'
+	AND test_name = 'web_connectivity'
+	GROUP BY 1,2,3,4
+) as wc
+GROUP BY 3,4,5;
+        """
+        c.execute('REFRESH MATERIALIZED VIEW ooexpl_wc_confirmed;')
+
 def meta_pg(in_root, bucket, postgres):
     print "meta_pg: {} {}".format(in_root, bucket)
 
