08a17f269fc515800ef968dccb7a8082d5430bbe
Arturo Filastò <arturo@filasto.net>
2019-06-03 13:24:55 +0200

    Add one-shot INSERT to comment of migration script

--- af/oometa/016-ooexpl_wc_confirmed.install.sql
+++ af/oometa/016-ooexpl_wc_confirmed.install.sql
@@ -5,13 +5,43 @@ select _v.register_patch( '016-ooexpl_wc_confirmed', ARRAY[ '015-fingerprint-fix
 /* Store precomputed `confirmed` count and `msm_count`
 from the web_connectivity table. Used by OONI Explorer. */
 
+/*
+INSERT INTO ooexpl_wc_confirmed
+SELECT
+COALESCE(SUM(CASE WHEN confirmed = TRUE THEN 1 ELSE 0 END), 0) as confirmed_count,
+COUNT(*) as msm_count,
+date_trunc('day', test_start_time) as test_day,
+probe_cc,
+probe_asn,
+bucket_date
+FROM (
+	SELECT
+	DISTINCT input as input,
+	test_start_time,
+	probe_cc,
+	probe_asn,
+    bucket_date,
+	bool_or(confirmed) as confirmed
+	FROM measurement
+	JOIN input ON input.input_no = measurement.input_no
+	JOIN report ON report.report_no = measurement.report_no
+	WHERE test_start_time < current_date - interval '1 day'
+	AND test_start_time > current_date - interval '31 day'
+	AND test_name = 'web_connectivity'
+	GROUP BY 1,2,3,4
+) as wc
+GROUP BY 3,4,5,6;
+*/
+
 CREATE TABLE ooexpl_wc_confirmed (
     confirmed_count BIGINT NOT NULL,
     msm_count BIGINT NOT NULL,
     test_day TIMESTAMP NOT NULL,
+    bucket_date DATE,
     probe_cc CHARACTER(2) NOT NULL,
     probe_asn INTEGER NOT NULL,
     CONSTRAINT unique_day_cc_asn UNIQUE (test_day, probe_cc, probe_asn)
   ) ;
+
 
 COMMIT;
