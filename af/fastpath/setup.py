5744ccf9b59c3910bf15e13e732b6f70b8dffe90
Federico Ceratto <federico@openobservatory.org>
2019-09-05 11:11:39 +0100

    Rename module

--- af/fastpath/setup.py
+++ af/fastpath/setup.py
@@ -6,7 +6,6 @@ from setuptools import setup
 # Package meta-data.
 NAME = 'fastpath'
 DESCRIPTION = ''
-REQUIRES_PYTHON = '>=3.6.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -14,11 +13,12 @@ REQUIRED = [
 
 setup(
     name=NAME,
-    python_requires=REQUIRES_PYTHON,
+    python_requires='>=3.6.0',
     packages=["fastpath", "fastpath.tests"],
     entry_points={
-        'console_scripts': ['fastpath=fastpath.fastpath:main'],
+        'console_scripts': ['fastpath=fastpath.core:main'],
     },
     install_requires=REQUIRED,
-    include_package_data=True
+    include_package_data=True,
+    zip_safe=False
 )
