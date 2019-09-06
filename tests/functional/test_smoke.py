cfe9bf321385cc193dbb185fbbb3c260acf541b0
Arturo Filastò <arturo@filasto.net>
2019-09-06 19:23:03 +0200

    Add basic smoke test for input_cte

--- tests/functional/test_smoke.py
+++ tests/functional/test_smoke.py
@@ -4,3 +4,6 @@ def test_api_smoke(client):
 
     response = client.get("/api/v1/measurements")
     assert response.status_code == 200
+
+    response = client.get("/api/v1/measurements?domain=ooni.io")
+    assert response.status_code == 200
