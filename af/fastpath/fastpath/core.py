125eeeb701f86ec7cb56bd19e01231a5a12a3d48
Arturo Filastò <arturo@filasto.net>
2019-10-05 15:02:49 +0200

    Add a portable Queue implementation to work on macOS
    See: https://github.com/vterron/lemon/commit/9ca6b4b

--- af/fastpath/fastpath/core.py
+++ af/fastpath/fastpath/core.py
@@ -36,6 +36,7 @@ import fastpath.s3feeder as s3feeder
 import fastpath.db as db
 
 from fastpath.metrics import setup_metrics
+from fastpath.portable_queue import Queue
 
 import fastpath.utils
 
@@ -508,7 +509,7 @@ def core():
     scores = None
 
     # Spawn worker processes
-    queue = mp.Queue()
+    queue = Queue()
     workers = [
         mp.Process(target=msm_processor, args=(queue,)) for n in range(NUM_WORKERS)
     ]
