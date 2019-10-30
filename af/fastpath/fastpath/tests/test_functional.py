cf62f01544dbdb356a1430ce7126e3cbafee37ad
Federico Ceratto <federico@openobservatory.org>
2019-10-30 20:30:44 +0000

    Add functional test for Telegram

--- af/fastpath/fastpath/tests/test_functional.py
+++ af/fastpath/fastpath/tests/test_functional.py
@@ -13,6 +13,7 @@ import boto3  # debdeps: python3-boto3
 import pytest  # debdeps: python3-pytest
 
 import fastpath.core as fp
+import fastpath.s3feeder as s3feeder
 
 # from fastpath.utils import fingerprints
 
@@ -35,6 +36,10 @@ def cans():
     _cans = dict(
         it="2018-05-07/20180501T071932Z-IT-AS198471-web_connectivity-20180506T090836Z_AS198471_gKqEpbg0Ny30ldGCQockbZMJSg9HhFiSizjey5e6JxSEHvzm7j-0.2.0-probe.json.lz4",
         cn="2018-05-07/20180506T014008Z-CN-AS4134-web_connectivity-20180506T014010Z_AS4134_ZpxhAVt3iqCjT5bW5CfJspbqUcfO4oZfzDVjCWAu2UuVkibFsv-0.2.0-probe.json.lz4",
+        telegram="2019-08-29/telegram.0.tar.lz4",
+        # telegram="2019-08-29/20190829T105210Z-IR-AS31549-telegram-20190829T105214Z_AS31549_t32ZZ5av3B6yNruRIFhCnuT1dHTnwPk7vwIa9F0TAe064HG4tk-0.2.0-probe.json",
+        # whatsapp="2019-06-15/20190615T070248Z-ET-AS24757-whatsapp-20190615T070253Z_AS24757_gRi6dhAqgWa7Yp4tah4LX6Rl1j6c8kJuja3OgZranEpMicEj2p-0.2.0-probe.json",
+        # fb="2019-06-27/20190627T214121Z-ET-AS24757-facebook_messenger-20190627T214126Z_AS24757_h8g9P5kTmmzyX1VyOjqcVonIbFNujm84l2leMCwC2gX3BI78fI-0.2.0-probe.json",
     )
     for k, v in _cans.items():
         _cans[k] = Path("testdata") / v
@@ -76,3 +81,70 @@ def setup_module(module):
     fp.conf.cachedir = fp.conf.vardir / "cache"
     fp.conf.s3cachedir = fp.conf.cachedir / "s3"
     fp.conf.sshcachedir = fp.conf.cachedir / "ssh"
+
+
+def test_telegram(cans):
+    can = cans["telegram"]
+    for measurement_tup in s3feeder.load_multiple(can.as_posix()):
+        msm_jstr, msm = measurement_tup
+        if msm is None:
+            msm = ujson.loads(msm_jstr)
+        summary = fp.score_measurement(msm, [])
+        # fmt: off
+        if (msm["report_id"] == "20190830T002837Z_AS209_3nMvNkLIqSZMLqRiaiQylAuHxu6qpK7rVJcAA9Dv2UpcNMhPH0"):
+            assert summary["scores"] == {
+                "blocking_general": 1.5,
+                "blocking_global": 0.0,
+                "blocking_country": 0.0,
+                "blocking_isp": 0.0,
+                "blocking_local": 0.0,
+                "web_failure": None,
+                "accessible_endpoints": 10,
+                "unreachable_endpoints": 0,
+                "http_success_cnt": 0,
+                "http_failure_cnt": 0,
+            }, msm
+
+        elif (msm["report_id"] == "20190829T205910Z_AS45184_0TVMQZLWjkfOdqA5b5nNF1XHrafTD4H01GnVTwvfzfiLyLc45r"):
+            assert summary["scores"] == {
+                "blocking_general": 1.0,
+                "blocking_global": 0.0,
+                "blocking_country": 0.0,
+                "blocking_isp": 0.0,
+                "blocking_local": 0.0,
+                "web_failure": "connection_reset",
+                "accessible_endpoints": 10,
+                "unreachable_endpoints": 0,
+                "http_success_cnt": 10,
+                "http_failure_cnt": 0,
+                "msg": "Telegam failure: connection_reset",
+            }
+        elif (msm["report_id"] == "20190829T210302Z_AS197207_28cN0a47WSIxF3SZlXvceoLCSk3rSkyeg0n07pKGAi7XYyEQXM"):
+            assert summary["scores"] == {
+                "blocking_general": 3.0,
+                "blocking_global": 0.0,
+                "blocking_country": 0.0,
+                "blocking_isp": 0.0,
+                "blocking_local": 0.0,
+                "web_failure": "generic_timeout_error",
+                "accessible_endpoints": 0,
+                "unreachable_endpoints": 10,
+                "http_success_cnt": 0,
+                "http_failure_cnt": 10,
+                "msg": "Telegam failure: generic_timeout_error",
+            }
+        elif (msm["report_id"] == "20190829T220118Z_AS16345_28eP4Hw7PQsLmb4eEPWitNvIZH8utHddaTbWZ9qFcaZudmHPfz"):
+            assert summary["scores"] == {
+                "blocking_general": 3.0,
+                "blocking_global": 0.0,
+                "blocking_country": 0.0,
+                "blocking_isp": 0.0,
+                "blocking_local": 0.0,
+                "web_failure": "connect_error",
+                "accessible_endpoints": 0,
+                "unreachable_endpoints": 10,
+                "http_success_cnt": 0,
+                "http_failure_cnt": 10,
+                "msg": "Telegam failure: connect_error",
+            }
+        # fmt: on
