7bb760645f6383e8d2a8b171c1a4cdcec0b4528f
Federico Ceratto <federico@openobservatory.org>
2019-11-11 15:35:35 +0000

    Fail early on script or SQL error

--- scripts/travis_init_db.sh
+++ scripts/travis_init_db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -ex
+set -exuo pipefail
 tmpdir=$(mktemp -d)
 cd $tmpdir
 git clone --depth 1 https://github.com/ooni/pipeline.git
 psql -c 'create database ooni_measurements;' -U postgres
 echo "Creating database tables using SQL files:"
 ls pipeline/af/oometa/*.install.sql
-cat pipeline/af/oometa/*.install.sql | psql -U postgres ooni_measurements
+cat pipeline/af/oometa/*.install.sql | psql -U postgres ooni_measurements -v ON_ERROR_STOP=1
