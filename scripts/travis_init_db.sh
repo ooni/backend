4477e8ab2a88c64055df3788e15200e776efbaea
Federico Ceratto <federico@openobservatory.org>
2019-11-11 15:54:49 +0000

    Create amsapi role in Travis CI

--- scripts/travis_init_db.sh
+++ scripts/travis_init_db.sh
@@ -3,7 +3,13 @@ set -exuo pipefail
 tmpdir=$(mktemp -d)
 cd $tmpdir
 git clone --depth 1 https://github.com/ooni/pipeline.git
+
+echo "Create ooni_measurements database"
 psql -c 'create database ooni_measurements;' -U postgres
+
+echo "Create amsapi role"
+psql -U postgres ooni_measurements -c "CREATE ROLE amsapi;"
+
 echo "Creating database tables using SQL files:"
 ls pipeline/af/oometa/*.install.sql
 cat pipeline/af/oometa/*.install.sql | psql -U postgres ooni_measurements -v ON_ERROR_STOP=1
