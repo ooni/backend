47d6abc213bfe3e3573d33a06da526814bce4131
Arturo Filastò <arturo@filasto.net>
2019-12-04 18:51:59 +0200

    Drop page related unit tests

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -44,47 +44,6 @@ def test_pages_index(client):
     resp = client.get(url_for("pages.index"))
     assert resp.status_code == 200
 
-
-def test_pages_stats(client):
-    resp = client.get(url_for("pages.stats"))
-    assert resp.status_code == 200
-
-
-def test_pages_files_index(client):
-    resp = client.get(url_for("pages.files_index"))
-    assert resp.status_code == 200
-
-
-def test_pages_files_by_date_list(client):
-    resp = client.get(url_for("pages.files_by_date"))
-    assert resp.status_code == 200
-
-
-def test_pages_files_by_date_calendar(client):
-    resp = client.get(url_for("pages.files_by_date", view="calendar"))
-    assert resp.status_code == 200
-
-
-def test_pages_files_on_date(client):
-    resp = client.get(url_for("pages.files_on_date", date="2016-01-01"))
-    assert resp.status_code == 200
-
-
-def test_pages_files_by_country_list(client):
-    resp = client.get(url_for("pages.files_by_country"))
-    assert resp.status_code == 200
-
-
-def test_pages_files_by_country_flag(client):
-    resp = client.get(url_for("pages.files_by_country", view="flag"))
-    assert resp.status_code == 200
-
-
-def test_pages_files_in_country(client):
-    resp = client.get(url_for("pages.files_in_country", country_code="IT"))
-    assert resp.status_code == 200
-
-
 def test_pages_download_file_404(client):
     resp = client.get(url_for("pages.files_download", textname="DOES_NOT_EXIST"))
     assert resp.status_code == 404
