6c890c3352ba8960c724931271165044d3f00add
Federico Ceratto <federico@openobservatory.org>
2020-03-25 12:05:26 +0000

    Drop total_lag

--- af/analysis/analysis/analysis.py
+++ af/analysis/analysis/analysis.py
@@ -1101,8 +1101,7 @@ def monitor_measurement_creation(conf):
                         (pg_xlog_location_diff(pg_current_xlog_location(),sent_location) / 1024)::bigint as pending,
                         (pg_xlog_location_diff(sent_location,write_location) / 1024)::bigint as write,
                         (pg_xlog_location_diff(write_location,flush_location) / 1024)::bigint as flush,
-                        (pg_xlog_location_diff(flush_location,replay_location) / 1024)::bigint as replay,
-                        (pg_xlog_location_diff(pg_current_xlog_location(),replay_location))::bigint / 1024 as total_lag
+                        (pg_xlog_location_diff(flush_location,replay_location) / 1024)::bigint as replay
                         FROM pg_stat_replication"""
                     cur.execute(sql)
                     d = cur.fetchone()
