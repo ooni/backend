8050397f9a71584a3c2dd0a492da220767595768
Arturo Filastò <arturo@filasto.net>
2020-11-25 13:00:21 +0100

    Use pure css buttons

--- build_docs.py
+++ build_docs.py
@@ -165,19 +165,7 @@ def generate_github_link_unused(action, f, lineno):
 
 def generate_badge(url, text):
     tpl = """
-<a href="{url}" class="svg">
-  <object type="image/svg+xml">
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="33" height="20">
-<linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
-<stop offset="1" stop-opacity=".1"/></linearGradient>
-<clipPath id="a"><rect width="33" height="20" rx="3" fill="#fff"/></clipPath>
-<g clip-path="url(#a)"><path fill="#97ca00" d="M0 0h0v20H0z"/><path fill="#97ca00" d="M0 0h33v20H0z"/>
-<path fill="url(#b)" d="M0 0h33v20H0z"/></g>
-<g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="110">
-<text x="165" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="230">{text}</text>
-<text x="165" y="140" transform="scale(.1)" textLength="230">{text}</text></g></svg>
-  </object>
-</a>
+<a href="{url}" class="button button-small button-outline">{text}</a>
     """
     return tpl.format(url=url, text=text)
 
