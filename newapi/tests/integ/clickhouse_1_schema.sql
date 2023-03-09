7c5b6b614900e9f4e53401044828df84d54b7e99
Federico Ceratto <federico@debian.org>
2023-03-09 09:23:09 +0100

    Update tests DB setup

--- newapi/tests/integ/clickhouse_1_schema.sql
+++ newapi/tests/integ/clickhouse_1_schema.sql
@@ -163,3 +163,37 @@ CREATE TABLE msmt_feedback
 ENGINE = ReplacingMergeTree
 ORDER BY (measurement_uid, account_id)
 SETTINGS index_granularity = 4;
+
+CREATE TABLE default.fingerprints_dns
+(
+    `name` String,
+    `scope` Enum8('nat' = 1, 'isp' = 2, 'prod' = 3, 'inst' = 4, 'vbw' = 5, 'fp' = 6),
+    `other_names` String,
+    `location_found` String,
+    `pattern_type` Enum8('full' = 1, 'prefix' = 2, 'contains' = 3, 'regexp' = 4),
+    `pattern` String,
+    `confidence_no_fp` UInt8,
+    `expected_countries` String,
+    `source` String,
+    `exp_url` String,
+    `notes` String
+)
+ENGINE = EmbeddedRocksDB
+PRIMARY KEY name;
+
+CREATE TABLE default.fingerprints_http
+(
+    `name` String,
+    `scope` Enum8('nat' = 1, 'isp' = 2, 'prod' = 3, 'inst' = 4, 'vbw' = 5, 'fp' = 6, 'injb' = 7, 'prov' = 8),
+    `other_names` String,
+    `location_found` String,
+    `pattern_type` Enum8('full' = 1, 'prefix' = 2, 'contains' = 3, 'regexp' = 4),
+    `pattern` String,
+    `confidence_no_fp` UInt8,
+    `expected_countries` String,
+    `source` String,
+    `exp_url` String,
+    `notes` String
+)
+ENGINE = EmbeddedRocksDB
+PRIMARY KEY name;
