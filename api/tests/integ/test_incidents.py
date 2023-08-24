707a710109b359714a5f0f6ab22af857528a7b5c
Federico Ceratto <federico@debian.org>
2023-08-24 16:18:38 +0200

    Users publishing incidents returns an error

--- api/tests/integ/test_incidents.py
+++ api/tests/integ/test_incidents.py
@@ -154,7 +154,6 @@ def test_crud_general(cleanup, client, adminsession, usersession):
 
 
 def test_crud_user_create(cleanup, client, adminsession, usersession):
-    # Create. Users attempts to publish the incident but is prevented.
     title = "integ-test-2"
     new = dict(
         start_time=datetime(2020, 1, 1),
@@ -163,7 +162,7 @@ def test_crud_user_create(cleanup, client, adminsession, usersession):
         title=title,
         text="foo bar\nbaz\n",
         event_type="incident",
-        published=True,
+        published=False,
         CCs=["UK", "FR"],
         test_names=["web_connectivity"],
         ASNs=[1, 2],
@@ -204,6 +203,31 @@ def test_crud_user_create(cleanup, client, adminsession, usersession):
         "event_type": "incident",
     }
     assert i == expected
+
+
+def test_crud_user_create_cannot_publish(cleanup, client, adminsession, usersession):
+    # Create. Users attempts to publish the incident but is prevented.
+    title = "integ-test-3"
+    new = dict(
+        start_time=datetime(2020, 1, 1),
+        end_time=None,
+        reported_by="ooni",
+        title=title,
+        text="foo bar\nbaz\n",
+        event_type="incident",
+        published=True,
+        CCs=["UK", "FR"],
+        test_names=["web_connectivity"],
+        ASNs=[1, 2],
+        domains=[],
+        tags=["integ-test"],
+        links=[
+            "https://explorer.ooni.org/chart/mat?test_name=web_connectivity&axis_x=measurement_start_day&since=2023-04-16&until=2023-05-16&time_grain=day"
+        ],
+    )
+    d = dict(**new)
+    r = usersession.post("/api/v1/incidents/create", json=d)
+    assert r.status_code == 400, r.json
 
 
 def test_crud_invalid_fields(client, adminsession, usersession):
