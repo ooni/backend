7a3ee122a93603031ebfda8aba6f37325470b40b
Arturo Filastò <arturo@filasto.net>
2024-01-19 17:23:35 +0100

    Add support for returning the current version as string

--- api/fastapi/dataapi/main.py
+++ api/fastapi/dataapi/main.py
@@ -13,6 +13,21 @@ app = FastAPI()
 app.include_router(measurements.router, prefix="/api")
 app.include_router(aggregation.router, prefix="/api")
 
+from importlib.metadata import version as importlib_version
+from importlib.resources import files as importlib_files
+
+pkg_name = "dataapi"
+
+pkg_version = importlib_version(pkg_name)
+try:
+    with importlib_files("dataapi").joinpath("TAG_VERSION").open('r') as in_file:
+        tag_version = in_file.read().strip()
+except:
+    tag_version = None
+
+@app.get("/version")
+async def version():
+    return {"version": pkg_version, "tag_version": tag_version}
 
 @app.get("/")
 async def root():
