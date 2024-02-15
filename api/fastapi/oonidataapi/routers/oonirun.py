7724fa5af35e2db7a90c9068f90446eed849321f
Arturo Filastò <arturo@filasto.net>
2024-02-15 12:08:25 +0100

    Port OONI Run v2 API over to fastapi

--- api/fastapi/oonidataapi/routers/oonirun.py
+++ api/fastapi/oonidataapi/routers/oonirun.py
@@ -5,31 +5,37 @@ https://github.com/ooni/spec/blob/master/backends/bk-005-ooni-run-v2.md
 """
 
 from datetime import datetime
-from typing import Dict, Any
+from os import urandom
+from sys import byteorder
+from typing import Dict, Any, List, Optional
 import json
 import logging
 
-from flask import Blueprint, current_app, request, Response
+from fastapi import APIRouter, Depends, Query, HTTPException, Header
+from pydantic import BaseModel, constr
+from typing_extensions import Annotated
 
-from ooniapi.auth import (
+from ..config import settings, metrics
+
+from ..utils import (
+    commasplit,
+    query_click,
+    optimize_table,
+    insert_click,
+    raw_query,
     role_required,
     get_client_role,
     get_account_id_or_raise,
     get_account_id_or_none,
 )
-from ooniapi.config import metrics
-from ooniapi.database import query_click, optimize_table, insert_click, raw_query
-from ooniapi.errors import jerror
-from ooniapi.urlparams import commasplit
-from ooniapi.utils import nocachejson, cachedjson, generate_random_intuid
+from ..dependencies import ClickhouseClient, get_clickhouse_client
 
-from ooniapi.errors import InvalidRequest, EmptyTranslation
 
-log: logging.Logger
+log = logging.getLogger(__name__)
 
 # The table creation for CI purposes is in tests/integ/clickhouse_1_schema.sql
 
-oonirun_blueprint = Blueprint("oonirun_api", "oonirun")
+router = APIRouter()
 
 
 def from_timestamp(ts: str) -> datetime:
@@ -45,80 +51,73 @@ def to_db_date(t: datetime) -> str:
     return t.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
 
 
-def validate_translations_not_empty(descriptor: dict) -> None:
-    for f in ("description_intl", "short_description_intl", "name_intl"):
-        d = descriptor.get(f, {}) or {}
-        for lang, txn in d.items():
-            if txn == "":
-                raise EmptyTranslation()
+class OONIRunCreated(BaseModel):
+    ooni_run_link_id: int
+    v: int
 
 
-def compare_descriptors(previous_descriptor: dict, descriptor: dict) -> bool:
+class OONIRunDescriptor(BaseModel):
+    name: Annotated[str, constr(min_length=1)]
+    name_intl: Optional[Dict[str, Annotated[str, constr(min_length=1)]]]
+    short_description: Annotated[Optional[str], constr(min_length=1)]
+    short_description_intl: Optional[Dict[str, Annotated[str, constr(min_length=1)]]]
+
+    description: Annotated[Optional[str], constr(min_length=1)]
+    description_intl: Optional[Dict[str, Annotated[str, constr(min_length=1)]]]
+    icon: Optional[str]
+    author: Optional[str]
+    is_archived: Optional[bool]
+
+    nettests: List[Dict]
+
+
+def compare_descriptors(
+    previous_descriptor: OONIRunDescriptor, descriptor: OONIRunDescriptor
+) -> bool:
     """Return True if anything other than the localized fields changed"""
-    if previous_descriptor["nettests"] != descriptor["nettests"]:
+    if previous_descriptor.nettests != descriptor.nettests:
         return True
-    if previous_descriptor["author"] != descriptor["author"]:
+    if previous_descriptor.author != descriptor.author:
         return True
-    if previous_descriptor["icon"] != descriptor["icon"]:
+    if previous_descriptor.icon != descriptor.icon:
         return True
 
     return False
 
 
-@oonirun_blueprint.route("/api/_/ooni_run/create", methods=["POST"])
-@role_required(["admin", "user"])
-def create_oonirun() -> Response:
-    """Create a new oonirun link or a new version for an existing one.
-    ---
-    parameters:
-      - in: body
-        required: true
-        description: OONIRun descriptor content
-        schema:
-          type: object
-      #- id: oonirun id or null
-      #  description: used to create a new version of an existing OONIRun
-      #  in: query
-      #  type: string
-      #  required: false
-    responses:
-      '200':
-        schema:
-          type: object
-          properties:
-            ooni_run_link_id:
-              type: integer
-            v:
-              type: integer
-              description: response format version
-    """
-    global log
-    log = current_app.logger
+def generate_random_intuid() -> int:
+    collector_id = 0
+    randint = int.from_bytes(urandom(4), byteorder)
+    return randint * 100 + collector_id
+
+
+@router.post(
+    "/api/_/ooni_run/create",
+    tags=["oonirunv2"],
+    dependencies=[Depends(role_required(["admin", "user"]))],
+)
+def create_oonirun(
+    db: Annotated[ClickhouseClient, Depends(get_clickhouse_client)],
+    descriptor: OONIRunDescriptor,
+    ooni_run_link_id: Annotated[
+        Optional[int],
+        Query(description="ID of the OONI Run link ID being created"),
+    ] = None,
+    authorization: str = Header("authorization"),
+) -> OONIRunCreated:
+    """Create a new oonirun link or a new version for an existing one."""
     log.debug("creating oonirun")
-    account_id = get_account_id_or_raise()
-    descriptor = request.json
+    account_id = get_account_id_or_raise(authorization)
     assert descriptor
-    ooni_run_link_id_raw = request.args.get("ooni_run_link_id")
-
-    if descriptor.get("name", "") == "":
-        log.info("'name' field empty")
-        return jerror("'name' field must not be empty")
-
-    validate_translations_not_empty(descriptor)
-
-    desc_s = json.dumps(descriptor)
 
     now = datetime.utcnow()
-    now_ts = to_timestamp(now)
 
-    if ooni_run_link_id_raw is None:
+    if ooni_run_link_id is None:
         # Generate new ID
-        ooni_run_link_id = generate_random_intuid(current_app)
-        increase_descriptor_creation_time = True
+        ooni_run_link_id = generate_random_intuid()
+        descriptor_creation_time = now
 
     else:
-        # We need a previous oonirun belonging to the same user
-        ooni_run_link_id = int(ooni_run_link_id_raw)
         query = """SELECT descriptor, descriptor_creation_time
         FROM oonirun
         WHERE ooni_run_link_id = %(ooni_run_link_id)s AND creator_account_id = %(account_id)s
@@ -126,53 +125,54 @@ def create_oonirun() -> Response:
         LIMIT 1
         """
         query_params = dict(account_id=account_id, ooni_run_link_id=ooni_run_link_id)
-        q = query_click(query, query_params)
+        q = query_click(db, query, query_params)
         if not len(q):
-            return jerror("OONIRun descriptor not found")
+            raise HTTPException(status_code=404, detail="OONIRun descriptor not found")
 
         # A descriptor is already in the database and belongs to account_id
         # Check if we need to update the descriptor timestamp or only txn
-        previous_descriptor = json.loads(q[0]["descriptor"])
+        previous_descriptor = OONIRunDescriptor(**json.loads(q[0]["descriptor"]))
         increase_descriptor_creation_time = compare_descriptors(
             previous_descriptor, descriptor
         )
         del previous_descriptor
 
-        previous_descriptor_creation_time = q[0]["descriptor_creation_time"]
-
-    if increase_descriptor_creation_time:
-        descriptor_creation_time = now
-    else:
-        descriptor_creation_time = previous_descriptor_creation_time
+        if increase_descriptor_creation_time:
+            descriptor_creation_time = now
+        else:
+            descriptor_creation_time = q[0]["descriptor_creation_time"]
 
     row = dict(
-        author=descriptor["author"],
+        author=descriptor.author,
         creator_account_id=account_id,
-        descriptor=desc_s,
+        descriptor=json.dumps(descriptor),
         descriptor_creation_time=descriptor_creation_time,
         ooni_run_link_id=ooni_run_link_id,
-        name=descriptor["name"],
-        short_description=descriptor.get("short_description", ""),
+        name=descriptor.name,
+        short_description=descriptor.short_description or "",
         translation_creation_time=now,
-        icon=descriptor.get("icon", ""),
+        icon=descriptor.icon or "",
     )
-    log.info(
-        f"Inserting oonirun {ooni_run_link_id} {increase_descriptor_creation_time} {row}"
-    )
+    log.info(f"Inserting oonirun {ooni_run_link_id} {row}")
     sql_ins = """INSERT INTO oonirun (ooni_run_link_id, descriptor, creator_account_id,
         author, descriptor_creation_time, translation_creation_time, name,
         short_description, icon) VALUES"""
-    insert_click(sql_ins, [row])
+    insert_click(db, sql_ins, [row])
 
-    optimize_table("oonirun")
-    return nocachejson(v=1, ooni_run_link_id=ooni_run_link_id)
+    optimize_table(db, "oonirun")
+    return OONIRunCreated(v=1, ooni_run_link_id=ooni_run_link_id)
 
 
-@oonirun_blueprint.route(
-    "/api/_/ooni_run/archive/<int:ooni_run_link_id>", methods=["POST"]
+@router.post(
+    "/api/_/ooni_run/archive/{ooni_run_link_id}",
+    dependencies=[Depends(role_required(["admin", "user"]))],
+    tags=["oonirun"],
 )
-@role_required(["admin", "user"])
-def archive_oonirun(ooni_run_link_id) -> Response:
+def archive_oonirun(
+    db: Annotated[ClickhouseClient, Depends(get_clickhouse_client)],
+    ooni_run_link_id: int,
+    authorization: str = Header("authorization"),
+) -> OONIRunCreated:
     """Archive an OONIRun descriptor and all its past versions.
     ---
     parameters:
@@ -189,71 +189,53 @@ def archive_oonirun(ooni_run_link_id) -> Response:
               type: integer
               description: response format version
     """
-    global log
-    log = current_app.logger
     log.debug(f"archive oonirun {ooni_run_link_id}")
-    account_id = get_account_id_or_raise()
+    account_id = get_account_id_or_raise(authorization)
 
     # Async mutation on all servers
     query = "ALTER TABLE oonirun UPDATE archived = 1 WHERE ooni_run_link_id = %(ooni_run_link_id)s"
-    if get_client_role() != "admin":
+    if get_client_role(authorization) != "admin":
         query += " AND creator_account_id = %(account_id)s"
 
     query_params = dict(ooni_run_link_id=ooni_run_link_id, account_id=account_id)
-    raw_query(query, query_params)
-    optimize_table("oonirun")
-    return nocachejson(v=1)
+    raw_query(db, query, query_params)
+    optimize_table(db, "oonirun")
+    return OONIRunCreated(v=1, ooni_run_link_id=ooni_run_link_id)
+
+
+class OONIRunDescriptorFetch(BaseModel):
+    archived: bool
+    descriptor: OONIRunDescriptor
+    descriptor_creation_time: datetime
+    mine: bool
+    translation_creation_time: datetime
+    v: int
 
 
 @metrics.timer("fetch_oonirun_descriptor")
-@oonirun_blueprint.route(
-    "/api/_/ooni_run/fetch/<int:ooni_run_link_id>", methods=["GET"]
+@router.get(
+    "/api/_/ooni_run/fetch/{ooni_run_link_id}",
+    tags=["oonirun"],
 )
-def fetch_oonirun_descriptor(ooni_run_link_id) -> Response:
-    """Fetch OONIRun descriptor by creation time or the newest one
-    ---
-    parameters:
-      - name: ooni_run_link_id
-        in: path
-        type: integer
-        required: true
-      - name: creation_time or null
-        in: query
-        type: string
-        example: "2023-06-02T12:33:43.123Z"
-        required: false
-    responses:
-      '200':
-        description: OONIRun metadata and descriptor
-        schema:
-          type: object
-          properties:
-            archived:
-              type: boolean
-              description: the descriptor is archived
-            descriptor:
-              type: object
-              description: descriptor data
-            descriptor_creation_time:
-              type: string
-              description: descriptor creation time
-            mine:
-              type: boolean
-              description: the descriptor belongs to the logged-in user. Optional.
-            translation_creation_time:
-              type: string
-              description: translation creation time
-            v:
-              type: integer
-              description: response format version
-    """
+def fetch_oonirun_descriptor(
+    db: Annotated[ClickhouseClient, Depends(get_clickhouse_client)],
+    ooni_run_link_id: int,
+    creation_time: Annotated[
+        Optional[str],
+        Query(
+            description="filter by descriptor create time eg. `2023-06-02T12:33:43.123Z`"
+        ),
+    ] = None,
+    authorization: str = Header("authorization"),
+) -> OONIRunDescriptorFetch:
+    """Fetch OONIRun descriptor by creation time or the newest one"""
     # Return the latest version of the translations
-    global log
-    log = current_app.logger
     log.debug("fetching oonirun")
-    descriptor_creation_time = request.args.get("creation_time")
-    account_id = get_account_id_or_none()
-    query_params = dict(ooni_run_link_id=ooni_run_link_id, account_id=account_id)
+    descriptor_creation_time = creation_time
+    account_id = get_account_id_or_none(authorization)
+    query_params: Dict[str, Any] = dict(
+        ooni_run_link_id=ooni_run_link_id, account_id=account_id
+    )
     if descriptor_creation_time is None:
         # Fetch latest version
         creation_time_filter = ""
@@ -270,14 +252,14 @@ def fetch_oonirun_descriptor(ooni_run_link_id) -> Response:
         ORDER BY descriptor_creation_time DESC
         LIMIT 1
     """
-    q = query_click(query, query_params)
+    q = query_click(db, query, query_params)
     if not len(q):
-        return jerror("oonirun descriptor not found")
+        raise HTTPException(status_code=404, detail="OONIRun descriptor not found")
 
     r = q[0]
     descriptor = json.loads(r["descriptor"])
 
-    kw = dict(
+    return OONIRunDescriptorFetch(
         archived=bool(r["archived"]),
         descriptor=descriptor,
         descriptor_creation_time=r["descriptor_creation_time"],
@@ -285,71 +267,41 @@ def fetch_oonirun_descriptor(ooni_run_link_id) -> Response:
         translation_creation_time=r["translation_creation_time"],
         v=1,
     )
-    return cachedjson("1h", **kw)
 
 
-@oonirun_blueprint.route("/api/_/ooni_run/list", methods=["GET"])
-def list_oonirun_descriptors() -> Response:
-    """List OONIRun descriptors
-    ---
-    parameters:
-      - name: ooni_run_link_id
-        in: query
-        type: string
-        description: OONIRun descriptors comma separated
-      - name: only_latest
-        in: query
-        type: boolean
-      - name: only_mine
-        in: query
-        type: boolean
-      - name: include_archived
-        in: query
-        type: boolean
-    responses:
-      '200':
-        description: OONIRun metadata and descriptor
-        schema:
-          type: object
-          properties:
-            v:
-              type: integer
-              description: response format version
-            descriptors:
-              type: array
-              description: OONIRun descriptors metadata
-              items:
-                type: object
-                properties:
-                  ooni_run_link_id:
-                    type: string
-                    description: descriptor ID
-                  archived:
-                    type: boolean
-                  author:
-                    type: string
-                  creation_time:
-                    type: string
-                    description: descriptor creation time
-                  mine:
-                    type: boolean
-                    description: the descriptor belongs to the logged-in user. Optional.
-                  name:
-                    type: string
-                  short_description:
-                    type: string
-                  icon:
-                    type: string
-    """
-    global log
-    log = current_app.logger
+class OONIRunDescriptorList(BaseModel):
+    v: int
+    descriptors: List[OONIRunDescriptorFetch]
+
+
+@router.get("/api/_/ooni_run/list", tags=["oonirun"])
+def list_oonirun_descriptors(
+    db: Annotated[ClickhouseClient, Depends(get_clickhouse_client)],
+    ooni_run_link_id: Annotated[
+        Optional[str],
+        Query(description="OONI Run descriptors comma separated"),
+    ] = None,
+    only_latest: Annotated[
+        Optional[bool],
+        Query(description="List only the latest versions"),
+    ] = None,
+    only_mine: Annotated[
+        Optional[bool],
+        Query(description="List only the my descriptors"),
+    ] = None,
+    include_archived: Annotated[
+        Optional[bool],
+        Query(description="List also archived descriptors"),
+    ] = None,
+    authorization: str = Header("authorization"),
+) -> OONIRunDescriptorList:
+    """List OONIRun descriptors"""
     log.debug("list oonirun")
-    account_id = get_account_id_or_none()
+    account_id = get_account_id_or_none(authorization)
 
     query_params: Dict[str, Any] = dict(account_id=account_id)
     try:
         filters = []
-        only_latest = bool(request.args.get("only_latest"))
         if only_latest:
             filters.append(
                 """
@@ -360,20 +312,16 @@ def list_oonirun_descriptors() -> Response:
                 GROUP BY ooni_run_link_id
             )"""
             )
-
-        include_archived = bool(request.args.get("include_archived"))
         if not include_archived:
             filters.append(
                 """
             archived = 0
             """
             )
-
-        only_mine = bool(request.args.get("only_mine"))
         if only_mine:
             filters.append("creator_account_id = %(account_id)s")
 
-        ids_s = request.args.get("ooni_run_link_id")
+        ids_s = ooni_run_link_id
         if ids_s:
             ids = commasplit(ids_s)
             filters.append("ooni_run_link_id IN %(ids)s")
@@ -386,7 +334,7 @@ def list_oonirun_descriptors() -> Response:
 
     except Exception as e:
         log.debug(f"list_oonirun_descriptors: invalid parameter. {e}")
-        return jerror("Incorrect parameter used")
+        raise HTTPException(status_code=400, detail="Incorrect parameter used")
 
     if account_id is None:
         mine_col = "0"
@@ -404,9 +352,9 @@ def list_oonirun_descriptors() -> Response:
     {fil}
     ORDER BY descriptor_creation_time, translation_creation_time
     """
-    descriptors = list(query_click(query, query_params))
+    descriptors = list(query_click(db, query, query_params))
     for d in descriptors:
         d["mine"] = bool(d["mine"])
         d["archived"] = bool(d["archived"])
     log.debug(f"Returning {len(descriptors)} descriptor[s]")
-    return nocachejson(v=1, descriptors=descriptors)
+    return OONIRunDescriptorList(v=1, descriptors=descriptors)
--- api/fastapi/oonidataapi/utils.py
+++ api/fastapi/oonidataapi/utils.py
@@ -1,9 +1,11 @@
 from csv import DictWriter
 from io import StringIO
 import logging
-from typing import Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union
+from fastapi import HTTPException, Header
 from fastapi.responses import JSONResponse
 
+import jwt
 import clickhouse_driver
 import clickhouse_driver.errors
 
@@ -103,3 +105,120 @@ def query_click_one_row(
         return dict(zip(colnames, row))  # type: ignore
 
     return None
+
+
+def insert_click(db: clickhouse_driver.Client, query: Query, rows: list) -> int:
+    assert isinstance(rows, list)
+    settings = {"priority": 1, "max_execution_time": 300}  # query_prio
+    return db.execute(query, rows, types_check=True, settings=settings)  # type: ignore
+
+
+def optimize_table(db: clickhouse_driver.Client, tblname: str) -> None:
+    settings = {"priority": 1, "max_execution_time": 300}  # query_prio
+    sql = f"OPTIMIZE TABLE {tblname} FINAL"
+    db.execute(sql, {}, settings=settings)
+
+
+def raw_query(
+    db: clickhouse_driver.Client, query: Query, query_params: dict, query_prio=1
+):
+    settings = {"priority": query_prio, "max_execution_time": 300}
+    q = db.execute(query, query_params, with_column_types=True, settings=settings)
+    return q
+
+
+def get_client_token() -> Optional[Dict]:
+    # Returns decoded JWT from client or raises ExpiredSignatureError
+    try:
+        bt = request.headers.get("Authorization", "")
+        if bt.startswith("Bearer "):
+            token = bt[7:]
+
+        return decode_jwt(token, audience="user_auth")
+    except Exception:
+        return None
+
+
+def decode_jwt(token: str, **kw) -> Dict[str, Any]:
+    # raises ExpiredSignatureError on expiration
+    key = settings.jwt_encryption_key
+    tok = jwt.decode(token, key, algorithms=["HS256"], **kw)
+    return tok
+
+
+def get_client_token(authorization: str):
+    try:
+        assert authorization.startswith("Bearer ")
+        token = authorization[7:]
+        return decode_jwt(token, audience="user_auth")
+    except:
+        return None
+
+
+def role_required(roles):
+    """Wrapped function requiring user to be logged in and have the right role."""
+    # Also:
+    #  explicitely set no-cache headers
+    #  apply the cross_origin decorator to:
+    #    - set CORS header to a trusted URL
+    #    - enable credentials (cookies)
+    #
+    if isinstance(roles, str):
+        roles = [roles]
+
+    async def verify_jwt(authorization: str = Header("authorization")):
+        tok = get_client_token(authorization)
+        if tok is None:
+            raise HTTPException(detail="Authentication required", status_code=401)
+        if tok["role"] not in roles:
+            raise HTTPException(detail="Role not authorized", status_code=401)
+
+        # TODO(art): we don't check for the session_expunge table yet. It's empty so the impact is none
+        # query = """SELECT threshold
+        #    FROM session_expunge
+        #    WHERE account_id = :account_id """
+        # account_id = tok["account_id"]
+        # query_params = dict(account_id=account_id)
+        # row = query_click_one_row(sql.text(query), query_params)
+        # if row:
+        #    threshold = row["threshold"]
+        #    iat = datetime.utcfromtimestamp(tok["iat"])
+        #    if iat < threshold:
+        #        return jerror("Authentication token expired", 401)
+
+        # If needed we can add here a 2-tier expiration time: long for
+        # /api/v1/user_refresh_token and short for everything else
+
+    return verify_jwt
+
+
+def get_client_role(authorization: str) -> str:
+    """Raise exception for unlogged users"""
+    tok = get_client_token(authorization)
+    assert tok
+    return tok["role"]
+
+
+def get_account_id_or_none(authorization: str) -> Optional[str]:
+    """Returns None for unlogged users"""
+    tok = get_client_token(authorization)
+    if tok:
+        return tok["account_id"]
+    return None
+
+
+def get_account_id_or_raise(authorization: str) -> str:
+    """Raise exception for unlogged users"""
+    tok = get_client_token(authorization)
+    if tok:
+        return tok["account_id"]
+    raise Exception
+
+
+def get_account_id(authorization: str):
+    # TODO: switch to get_account_id_or_none
+    tok = get_client_token(authorization)
+    if not tok:
+        return jerror("Authentication required", 401)
+
+    return tok["account_id"]
