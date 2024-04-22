211e566ba3853a2997aa01898c605e725b5c949e
Arturo Filastò <arturo@filasto.net>
2024-04-22 10:42:00 +0200

    Remove rotation script and add 4.th to the roundrobin (#836)
    * Remove th rotation script and systemd unit
    
    * Add 4.th.ooni.org to the round robin
    
    * Fix version number
    
    * Unmerged PR https://github.com/ooni/backend/pull/744/files had already claimed version 1.11
    
    * Fix bug in round_robin
    
    * Bump ooni-api changelog too

--- api/ooniapi/probe_services.py
+++ api/ooniapi/probe_services.py
@@ -512,11 +512,11 @@ def round_robin_web_test_helpers() -> List[Dict]:
     if q < 10:
         shift = 0
     else:
-        shift = q % 3 + 1
+        shift = q % 4 + 1
 
     out = []
-    for n in range(4):
-        n = (n + shift) % 4
+    for n in range(5):
+        n = (n + shift) % 5
         out.append({"address": f"https://{n}.th.ooni.org", "type": "https"})
 
     return out
