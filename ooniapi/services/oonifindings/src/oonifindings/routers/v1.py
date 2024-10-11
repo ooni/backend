dc4e4d3b14b6c1721b74418a47bf95b98a2a5315
Arturo Filastò <arturo@filasto.net>
2024-10-11 16:53:12 +0200

    Make publish behave like current backend

--- ooniapi/services/oonifindings/src/oonifindings/routers/v1.py
+++ ooniapi/services/oonifindings/src/oonifindings/routers/v1.py
@@ -493,6 +493,10 @@ def delete_oonifinding(
     return {}
 
 
+class OONIFindingsPublish(BaseModel):
+    id: str = Field(title="ID of the finding")
+
+
 @router.post(
     "/v1/incidents/{action}",
     tags=["oonifindings"],
@@ -501,7 +505,7 @@ def delete_oonifinding(
 )
 def update_oonifinding_publish_status(
     action: str,
-    publish_request: OONIFindingCreateUpdate,
+    publish_request: OONIFindingsPublish,
     response: Response,
     db=Depends(get_postgresql_session),
 ):
@@ -512,7 +516,7 @@ def update_oonifinding_publish_status(
         raise HTTPException(status_code=400, detail="Invalid query action")
 
     assert publish_request
-    finding_id = publish_request.incident_id
+    finding_id = publish_request.id
 
     q = db.query(models.OONIFinding).filter(models.OONIFinding.finding_id == finding_id)
 
