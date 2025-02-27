423df2f3f0ac0336acafa576b3dff61e01a98881
Luis Díaz <ldiazn98@gmail.com>
2025-02-27 17:28:56 +0100

    use supervisord to manage internal processes

---
+++ ooniapi/services/oonirun/test.py
@@ -0,0 +1,4 @@
+import ooniprobe
+import ujson
+
+import ooniprobe.dist
--- ooniapi/services/reverseproxy/scripts/40-start-prom-exporter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script is used to start the prometheus exporter for nginx.
-# More about the exporter here: 
-# https://github.com/nginx/nginx-prometheus-exporter
-
-nginx-prometheus-exporter --nginx.scrape-uri=http://localhost:8080/stub_status &
