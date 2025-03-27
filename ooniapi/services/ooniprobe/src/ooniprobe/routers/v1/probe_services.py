9b86df42380d78589c658660a306750f08b82902
Luis Diaz <41093870+LDiazN@users.noreply.github.com>
2025-03-27 13:02:25 +0100

    Bg geoip update (#951)
    * Add checkin models and initial route
    
    * Porting old code to new probe_services endpoint
    
    * Add geoip2 dependency
    
    * Add geoip tables as dependencies
    
    * Ported probe_geoip function
    
    * Porting more functions from checkin
    
    * Added health checks for clickhouse
    
    * First complete version of checkin endpoint port
    
    * Fix bad name for metric
    
    * Port some checkin integration tests
    
    * Add pytest-docker as test dependency
    
    * Add docker compose to launch clickhouse db for tests
    
    * Making tests work with the new setup
    
    * Fixed unhashable settings error
    
    * ported script to download geoip DB
    
    * Remove exit
    
    * Use setting to fetch db dir
    
    * Added setting for geoip db dir location
    
    * Rename download geoip file
    
    * Set download dir as input argument
    
    * download db before tests
    
    * Black reformat
    
    * fix error with bad header parsing
    
    * Making tests work
    
    * Add settings as a parameter to mock it in tests
    
    * Fix bad reader dependency
    
    * Skip broken test
    
    * Add function to populate db
    
    * Add data for tests
    
    * Fix fixture loading
    
    * Add clickhouse db fixture
    
    * Adding integration tests for check in
    
    * Removed todo comments
    
    * Add missin / at the start of path
    
    * Black reformat
    
    * Refactor to move prio code into a prio.py file
    
    * Add todo comment
    
    * Add timer to measure function run time
    
    * Improved writing of comment
    
    * Improved writing of comment
    
    * Added missing alembic migration
    
    * Revert "Added missing alembic migration"
    
    This reverts commit fb072d21e0da7c2b1e1a9d01093457c35515dab6.
    
    * Add help comment in init.sh
    
    * Black reformat
    
    * Add APScheduler for background tasks
    
    * Add geoip db update job
    
    * Changed APScheduler for fast api utils repeat-every
    
    * Add repeating task for updating the geoip DB
    
    * Add parameter to enable/disable repeating tasks
    
    The tests were hanging because of the repeating tasks, so I added
    a parameter to deactivate them in tests
    
    * Change task execution frequency
    
    * Remove print
    
    * Black reformat
    
    * Add TS calculation to function body
    
    * Re-raise consistency check exception in download_geoip
    
    * Add geoip data dir fixture
    
    * Removed unnecessary remove
    
    * Changed heavy fastpath query on health check
    
    * trigger ci
    
    * Reformat query
    
    * Changed health check to a less heavy query

--- ooniapi/services/ooniprobe/src/ooniprobe/routers/v1/probe_services.py
+++ ooniapi/services/ooniprobe/src/ooniprobe/routers/v1/probe_services.py
@@ -1,17 +1,26 @@
+from base64 import b64encode
+from os import urandom
 import logging
 from datetime import datetime, timezone, timedelta
 import time
-from typing import List, Optional
+from typing import List, Optional, Any, Dict, Tuple, Annotated, TypeAlias, Optional
+import random
 
-from fastapi import APIRouter, Depends, HTTPException, Response
-from prometheus_client import Counter, Info
+import geoip2
+import geoip2.errors
+from fastapi import APIRouter, Depends, HTTPException, Response, Request, Header
+from prometheus_client import Counter, Info, Gauge
 from enum import Enum
+import sqlalchemy as sa
+from clickhouse_driver import Client as Clickhouse
 
+from ...dependencies import CCReaderDep, ASNReaderDep, ClickhouseDep, SettingsDep
 from ...common.dependencies import get_settings
 from ...common.routers import BaseModel
 from ...common.auth import create_jwt, decode_jwt, jwt
 from ...common.config import Settings
 from ...common.utils import setnocacheresponse
+from ...prio import generate_test_list
 
 router = APIRouter(prefix="/v1")
 
@@ -28,6 +37,28 @@ class Metrics:
     PROBE_UPDATE_INFO = Info(
         "probe_update_info",
         "Information reported in the probe update endpoint",
+    )
+
+    CHECK_IN_TEST_LIST_COUNT = Gauge(
+        "check_in_test_list_count", "Amount of test lists present in each experiment"
+    )
+
+    GEOIP_ADDR_FOUND = Counter(
+        "geoip_ipaddr_found",
+        "If the ip address was found by geoip",
+        labelnames=["probe_cc", "asn"],
+    )
+
+    GEOIP_ADDR_NOT_FOUND = Counter(
+        "geoip_ipaddr_not_found", "We couldn't look up the IP address in the database"
+    )
+
+    GEOIP_CC_DIFFERS = Counter(
+        "geoip_cc_differs", "There's a mismatch between reported CC and observed CC"
+    )
+
+    GEOIP_ASN_DIFFERS = Counter(
+        "geoip_asn_differs", "There's a mismatch between reported ASN and observed ASN"
     )
 
 
@@ -202,3 +233,362 @@ def probe_update_post(probe_update: ProbeUpdate) -> ProbeUpdateResponse:
     Metrics.PROBE_UPDATE_INFO.info(probe_update_dict)
 
     return ProbeUpdateResponse(status="ok")
+
+
+class CheckIn(BaseModel):
+    run_type: str = "timed"
+    charging: bool = True
+    probe_cc: str = "ZZ"
+    probe_asn: str = "AS0"
+    on_wifi: bool = False
+    charging: bool = False
+    software_name: str = ""
+    software_version: str = ""
+    web_connectivity: Optional[Dict[str, Any]] = None
+
+
+class CheckInResponse(BaseModel):
+    """
+    v:
+      type: integer
+      description: response format version
+    probe_cc:
+      type: string
+      description: probe CC inferred from GeoIP or ZZ
+    probe_asn:
+      type: string
+      description: probe ASN inferred from GeoIP or AS0
+    probe_network_name:
+      type: string
+      description: probe network name inferred from GeoIP or None
+    utc_time:
+      type: string
+      description: current UTC time as YYYY-mm-ddTHH:MM:SSZ
+    conf:
+      type: object
+      description: auxiliary configuration parameters
+      features:
+        type: object
+        description: feature flags
+    tests:
+      type: object
+      description: test-specific configuration
+      properties:
+        web_connectivity:
+          type: object
+          properties:
+            report_id:
+              type: string
+            urls:
+              type: array
+              items:
+                type: object
+                properties:
+                  category_code:
+                    type: string
+                  country_code:
+                    type: string
+                  url:
+                    type: string
+    """
+
+    v: int
+    probe_cc: str
+    probe_asn: str
+    probe_network_name: Optional[str]
+    utc_time: str
+    conf: Dict[str, Any]
+    tests: Dict[str, Any]
+
+
+@router.post("/check-in", tags=["ooniprobe"])
+def check_in(
+    request: Request,
+    response: Response,
+    check_in: CheckIn,
+    cc_reader: CCReaderDep,
+    asn_reader: ASNReaderDep,
+    clickhouse: ClickhouseDep,
+    settings: SettingsDep,
+) -> CheckInResponse:
+
+    # TODO: Implement throttling
+    run_type = check_in.run_type
+    charging = check_in.charging
+    probe_cc = check_in.probe_cc.upper()
+    probe_asn = check_in.probe_asn
+    software_name = check_in.software_name
+    software_version = check_in.software_version
+
+    resp, probe_cc, asn_i = probe_geoip(
+        request,
+        probe_cc,
+        probe_asn,
+        cc_reader,
+        asn_reader,
+    )
+
+    # On run_type=manual preserve the old behavior: test the whole list
+    # On timed runs test few URLs, especially when on battery
+    if run_type == "manual":
+        url_limit = 9999  # same as prio.py
+    elif charging:
+        url_limit = 100
+    else:
+        url_limit = 20
+
+    try:
+        charging = bool(charging)
+    except Exception:
+        log.error(
+            f"check-in params: {url_limit} '{probe_cc}' '{charging}' '{run_type}' '{software_name}' '{software_version}'"
+        )
+
+    if check_in.web_connectivity is not None:
+        catcodes = check_in.web_connectivity.get("category_codes", [])
+        if isinstance(catcodes, str):
+            category_codes = catcodes.split(",")
+        else:
+            category_codes = catcodes
+
+    else:
+        category_codes = []
+
+    for c in category_codes:
+        assert c.isalpha()
+
+    try:
+        test_items, _1, _2 = generate_test_list(
+            clickhouse, probe_cc, category_codes, asn_i, url_limit, False
+        )
+    except Exception as e:
+        log.error(e, exc_info=True)
+        # TODO: use same failover as prio.py:list_test_urls
+        # failover_generate_test_list runs without any database interaction
+        # test_items = failover_generate_test_list(country_code, category_codes, limit)
+        test_items = []
+
+    Metrics.CHECK_IN_TEST_LIST_COUNT.set(len(test_items))
+
+    conf: Dict[str, Any] = dict(
+        features={
+            # TODO(https://github.com/ooni/probe-cli/pull/1522): we disable torsf until we have
+            # addressed the issue with the fast.ly based rendezvous method being broken
+            "torsf_enabled": False,
+            "vanilla_tor_enabled": True,
+        }
+    )
+
+    # set webconnectivity_0.5 feature flag for some probes
+    # Temporarily disabled while we work towards deploying this in prod:
+    # https://github.com/ooni/probe/issues/2674
+    #
+    # octect = extract_probe_ipaddr_octect(1, 0)
+    # if octect in (34, 239):
+    #    conf["features"]["webconnectivity_0.5"] = True
+
+    conf["test_helpers"] = generate_test_helpers_conf()
+
+    resp["tests"] = {
+        "web_connectivity": {"urls": test_items},
+    }
+    resp["conf"] = conf
+    resp["utc_time"] = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    test_names = (
+        "bridge_reachability",
+        "dash",
+        "dns_consistency",
+        "dnscheck",
+        "facebook_messenger",
+        "http_header_field_manipulation",
+        "http_host",
+        "http_invalid_request_line",
+        "http_requests",
+        "meek_fronted_requests_test",
+        "multi_protocol_traceroute",
+        "ndt",
+        "psiphon",
+        "riseupvpn",
+        "tcp_connect",
+        "telegram",
+        "tor",
+        "urlgetter",
+        "vanilla_tor",
+        "web_connectivity",
+        "whatsapp",
+    )
+    for tn in test_names:
+        rid = generate_report_id(tn, settings, probe_cc, asn_i)
+        resp["tests"].setdefault(tn, {})  # type: ignore
+        resp["tests"][tn]["report_id"] = rid  # type: ignore
+
+    til = len(test_items)
+    log.debug(
+        f"check-in params: {url_limit} {til} '{probe_cc}' '{charging}' '{run_type}' '{software_name}' '{software_version}'"
+    )
+
+    setnocacheresponse(response)
+    checkin_response = CheckInResponse(
+        v=resp["v"],
+        probe_cc=resp["probe_cc"],
+        probe_asn=resp["probe_asn"],
+        probe_network_name=resp["probe_network_name"],
+        utc_time=resp["utc_time"],
+        conf=resp["conf"],
+        tests=resp["tests"],
+    )
+
+    return checkin_response
+
+
+def probe_geoip(
+    request: Request,
+    probe_cc: str,
+    asn: str,
+    cc_reader: CCReaderDep,
+    asn_reader: ASNReaderDep,
+) -> Tuple[Dict, str, int]:
+    """Looks up probe CC, ASN, network name using GeoIP, prepare
+    response dict
+    """
+    db_probe_cc = "ZZ"
+    db_asn = "AS0"
+    db_probe_network_name = None
+    try:
+        ipaddr = extract_probe_ipaddr(request)
+        db_probe_cc = lookup_probe_cc(ipaddr, cc_reader)
+        db_asn, db_probe_network_name = lookup_probe_network(ipaddr, asn_reader)
+        Metrics.GEOIP_ADDR_FOUND.labels(probe_cc=db_probe_cc, asn=db_asn).inc()
+    except geoip2.errors.AddressNotFoundError:
+        Metrics.GEOIP_ADDR_NOT_FOUND.inc()
+    except Exception as e:
+        log.error(str(e), exc_info=True)
+
+    if probe_cc != "ZZ" and probe_cc != db_probe_cc:
+        log.info(f"probe_cc != db_probe_cc ({probe_cc} != {db_probe_cc})")
+        Metrics.GEOIP_CC_DIFFERS.inc()
+    if asn != "AS0" and asn != db_asn:
+        log.info(f"probe_asn != db_probe_as ({asn} != {db_asn})")
+        Metrics.GEOIP_ASN_DIFFERS.inc()
+
+    # We always returns the looked up probe_cc and probe_asn to the probe
+    resp: Dict[str, Any] = dict(v=1)
+    resp["probe_cc"] = db_probe_cc
+    resp["probe_asn"] = db_asn
+    resp["probe_network_name"] = db_probe_network_name
+
+    # Don't override probe_cc or asn unless the probe has omitted these
+    # values. This is done because the IP address we see might not match the
+    # actual probe ipaddr in cases in which a circumvention tool is being used.
+    # TODO: eventually we should have the probe signal to the backend that it
+    # wants the lookup to be done by the backend and have it pass the public IP
+    # through a specific header.
+    if probe_cc == "ZZ" and asn == "AS0":
+        probe_cc = db_probe_cc
+        asn = db_asn
+
+    assert asn.startswith("AS")
+    asn_int = int(asn[2:])
+    assert probe_cc.isalpha()
+    assert len(probe_cc) == 2
+
+    return resp, probe_cc, asn_int
+
+
+def extract_probe_ipaddr(request: Request) -> str:
+
+    real_ip_headers = ["X-Forwarded-For", "X-Real-IP"]
+    for h in real_ip_headers:
+        if h in request.headers:
+            return request.headers.getlist(h)[0].rpartition(" ")[-1]
+
+    return request.client.host if request.client else ""
+
+
+def lookup_probe_network(ipaddr: str, asn_reader: ASNReaderDep) -> Tuple[str, str]:
+    resp = asn_reader.asn(ipaddr)
+
+    return (
+        "AS{}".format(resp.autonomous_system_number),
+        resp.autonomous_system_organization or "0",
+    )
+
+
+def lookup_probe_cc(ipaddr: str, cc_reader: CCReaderDep) -> str:
+    resp = cc_reader.country(ipaddr)
+    return resp.country.iso_code or "ZZ"
+
+
+def generate_test_helpers_conf() -> Dict:
+    # Load-balance test helpers deterministically
+    conf = {
+        "dns": [
+            {"address": "37.218.241.93:57004", "type": "legacy"},
+            {"address": "37.218.241.93:57004", "type": "legacy"},
+        ],
+        "http-return-json-headers": [
+            {"address": "http://37.218.241.94:80", "type": "legacy"},
+            {"address": "http://37.218.241.94:80", "type": "legacy"},
+        ],
+        "ssl": [
+            {"address": "https://37.218.241.93", "type": "legacy"},
+            {"address": "https://37.218.241.93", "type": "legacy"},
+        ],
+        "tcp-echo": [
+            {"address": "37.218.241.93", "type": "legacy"},
+            {"address": "37.218.241.93", "type": "legacy"},
+        ],
+        "traceroute": [
+            {"address": "37.218.241.93", "type": "legacy"},
+            {"address": "37.218.241.93", "type": "legacy"},
+        ],
+        "web-connectivity": [
+            {"address": "httpo://o7mcp5y4ibyjkcgs.onion", "type": "legacy"},
+            {"address": "https://wcth.ooni.io", "type": "https"},
+            {
+                "address": "https://d33d1gs9kpq1c5.cloudfront.net",
+                "front": "d33d1gs9kpq1c5.cloudfront.net",
+                "type": "cloudfront",
+            },
+            {"address": "httpo://y3zq5fwelrzkkv3s.onion", "type": "legacy"},
+            {"address": "https://wcth.ooni.io", "type": "https"},
+            {
+                "address": "https://d33d1gs9kpq1c5.cloudfront.net",
+                "front": "d33d1gs9kpq1c5.cloudfront.net",
+                "type": "cloudfront",
+            },
+        ],
+    }
+    conf["web-connectivity"] = random_web_test_helpers(
+        [
+            "https://6.th.ooni.org",
+            "https://5.th.ooni.org",
+        ]
+    )
+    conf["web-connectivity"].append(
+        {
+            "address": "https://d33d1gs9kpq1c5.cloudfront.net",
+            "front": "d33d1gs9kpq1c5.cloudfront.net",
+            "type": "cloudfront",
+        }
+    )
+    return conf
+
+
+def random_web_test_helpers(th_list: List[str]) -> List[Dict]:
+    """Randomly sort test helpers"""
+    random.shuffle(th_list)
+    out = []
+    for th_addr in th_list:
+        out.append({"address": th_addr, "type": "https"})
+    return out
+
+
+def generate_report_id(test_name, settings: Settings, cc: str, asn_i: int) -> str:
+    ts = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%SZ")
+    cid = settings.collector_id
+    rand = b64encode(urandom(12), b"oo").decode()
+    stn = test_name.replace("_", "")
+    rid = f"{ts}_{stn}_{cc}_{asn_i}_n{cid}_{rand}"
+    return rid
