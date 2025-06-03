ec10d06491bdf151f2cb7448ec9338f532d51a9c
Luis Díaz <ldiazn98@gmail.com>
2025-06-03 11:44:15 +0200

    Add test for prioritization with measurements

--- ooniapi/services/oonirun/tests/integ/test_dynamic_lists.py
+++ ooniapi/services/oonirun/tests/integ/test_dynamic_lists.py
@@ -1,7 +1,7 @@
 from copy import deepcopy
 import json
 from pathlib import Path
-from oonirun.common.clickhouse_utils import insert_click
+from oonirun.common.clickhouse_utils import insert_click, query_click
 import pytest
 from ..test_oonirun import SAMPLE_OONIRUN, SAMPLE_META
 from datetime import datetime, timedelta, UTC
@@ -35,10 +35,10 @@ def url_priorities(clickhouse_db, fixtures_data_dir):
     clickhouse_db.execute("TRUNCATE TABLE url_priorities")
 
 def generate_random_date_last_7_days() -> datetime:
-    start = datetime.now(tz=UTC) + timedelta(days=7)
+    start = datetime.now(tz=UTC) - timedelta(days=7)
 
     # return a random date between 7 days ago and now
-    return start + timedelta(seconds=random.randrange(0, 3600 * 24 * 7))
+    return start + timedelta(seconds=random.randrange(3600 * 24, 3600 * 24 * 7 - 3600 * 24))
 
 @pytest.fixture(scope="module")
 def measurements(clickhouse_db, fixtures_data_dir):
@@ -60,7 +60,7 @@ def measurements(clickhouse_db, fixtures_data_dir):
 
 def test_engine_descriptor_basic(client, client_with_user_role, url_priorities):
     z = deepcopy(SAMPLE_OONIRUN)
-    z['name'] = "Testing header parsing"
+    z['name'] = "Testing simple prioritizationpri"
     z['nettests'][0]['targets_name'] = 'websites_list_prioritized'
     z['nettests'][0]['inputs'] = None
     z['nettests'][0]['inputs_extra'] = None
@@ -86,7 +86,7 @@ def test_check_in_url_category_news(client, client_with_user_role, url_prioritie
     Test that you can filter by category codes
     """
     z = deepcopy(SAMPLE_OONIRUN)
-    z['name'] = "Testing header parsing"
+    z['name'] = "Categories filtering"
     z['nettests'][0]['targets_name'] = 'websites_list_prioritized'
     z['nettests'][0]['inputs'] = None
     z['nettests'][0]['inputs_extra'] = None
@@ -107,7 +107,46 @@ def test_check_in_url_category_news(client, client_with_user_role, url_prioritie
     for extra in inputs_extra:
         assert extra["category_code"] == "NEWS"
 
-def test_priorization_with_measurements(client, client_with_user_role, url_priorities, measurements):
+def test_prioritization_with_measurements(client, client_with_user_role, url_priorities, measurements):
     """
     Test priorization including measurements
     """
+    z = deepcopy(SAMPLE_OONIRUN)
+    z['name'] = "Testing header parsing"
+    z['nettests'][0]['targets_name'] = 'websites_list_prioritized'
+    z['nettests'][0]['inputs'] = None
+    z['nettests'][0]['inputs_extra'] = None
+    z['nettests'] = z['nettests'][:1]
+    
+    # Create a link
+    j = postj(client_with_user_role, "/api/v2/oonirun/links", **z)
+    orlid = j['oonirun_link_id']
+
+
+    # fetch the link
+    meta = deepcopy(SAMPLE_META)
+    # In ES we have more measurements for twitter, (see tests/fixtures/data/measurements.json)
+    # so twitter should NOT show up first
+    meta['probe_cc'] = 'ES'
+    j = postj(client,f"/api/v2/oonirun/links/{orlid}/engine-descriptor/latest", **meta)
+    inputs = j["nettests"][0]["inputs"]
+    assert len(inputs), inputs
+    assert "twitter.com" not in inputs[0], "Twitter should not be the first one"
+
+    # Twitter with a different asn can be first 
+    meta['probe_cc'] = 'ES'
+    meta['probe_asn'] = 'AS9999'
+    j = postj(client,f"/api/v2/oonirun/links/{orlid}/engine-descriptor/latest", **meta)
+    inputs = j["nettests"][0]["inputs"]
+    assert len(inputs), inputs
+    assert "twitter.com" in inputs[0], "Twitter should be the first one"
+
+
+    # Similarly, in IT twitter should be first, and facebook last 
+    meta['probe_cc'] = 'IT'
+    meta['probe_asn'] = 'AS1234'
+    j = postj(client,f"/api/v2/oonirun/links/{orlid}/engine-descriptor/latest", **meta)
+    inputs = j["nettests"][0]["inputs"]
+    assert len(inputs), inputs
+    assert "twitter.com" in inputs[0], "Twitter should be the first one"
+    assert "facebook.com" in inputs[-1], "Facebook should be the last one"
