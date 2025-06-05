eeb97433f1b15b4dcd3315de923f831ddd555d2d
Luis Díaz <ldiazn98@gmail.com>
2025-06-05 10:27:07 +0200

    drop backend_options and options column

--- ooniapi/common/src/common/alembic/versions/b860eb79750f_add_targets_name_and_inputs_extra_.py
+++ ooniapi/common/src/common/alembic/versions/b860eb79750f_add_targets_name_and_inputs_extra_.py
@@ -5,6 +5,7 @@ Revises: 8e7ecea5c2f5
 Create Date: 2025-05-21 15:44:32.959349
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,19 +13,21 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'b860eb79750f'
-down_revision: Union[str, None] = '8e7ecea5c2f5'
+revision: str = "b860eb79750f"
+down_revision: Union[str, None] = "8e7ecea5c2f5"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column("oonirun_nettest", 
-        sa.Column("targets_name", sa.String(), nullable=True)
+    op.add_column(
+        "oonirun_nettest", sa.Column("targets_name", sa.String(), nullable=True)
     )
-    op.add_column("oonirun_nettest",
-        sa.Column("inputs_extra", sa.ARRAY(sa.JSON()), nullable=True)
+    op.add_column(
+        "oonirun_nettest", sa.Column("inputs_extra", sa.ARRAY(sa.JSON()), nullable=True)
     )
+    op.drop_column("oonirun_nettest", "backend_options")
+    op.drop_column("oonirun_nettest", "options")
 
 
 def downgrade() -> None:
