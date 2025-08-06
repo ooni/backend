de3fdbf2b666741f5e82669aa36e713612ef3427
Luis Díaz <ldiazn98@gmail.com>
2025-08-06 12:36:36 +0200

    Fix bad bucket name

--- ooniapi/common/src/common/config.py
+++ ooniapi/common/src/common/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     clickhouse_url: str = "clickhouse://localhost"
     postgresql_url: str = "postgresql://oonidb:oonidb@localhost/oonidb"
     log_level: str = "info"
-    s3_bucket_name: str = "oonidata-eufra"
+    s3_bucket_name: str = "ooni-data-eu-fra"
     other_collectors: List[str] = []
     statsd_host: str = "localhost"
     statsd_port: int = 8125
