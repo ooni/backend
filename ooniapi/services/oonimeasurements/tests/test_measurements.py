9782190491cbf71dd3d8800b464bba2f1dd18ab1
Luis Díaz <ldiazn98@gmail.com>
2025-09-30 12:13:26 +0200

    Working on test to reproduce bug

--- ooniapi/services/oonimeasurements/tests/test_measurements.py
+++ ooniapi/services/oonimeasurements/tests/test_measurements.py
@@ -246,3 +246,13 @@ def test_measurements_desc_default(client):
         next_d = get_time(row)
         assert next_d <= d, "Results should be in descending order"
         d = next_d
+
+def test_fix_msm_report_id_str(client):
+
+    resp = client.get("/api/v1/measurement_meta", params={
+        "report_id" : "20241209T185959Z_openvpn_MZ_37223_n1_x51ewx3QwQcqXNgW",
+        "full" : True,
+        "input" : "openvpn://oonivpn.corp?address=37.218.243.98%3A1194&transport=tcp"
+    })
+
+    assert resp.status_code == 200, resp.content
