fead5e2db4ea1748538c8440d50e3562de486c16
Luis Díaz <ldiazn98@gmail.com>
2025-10-27 16:27:54 +0100

    Add cusum changepoint list endpoint

---
+++ ooniapi/services/oonimeasurements/tests/test_event_detection.py
@@ -0,0 +1,10 @@
+from typing import Dict, Any
+import httpx
+
+def getj(client : httpx.Client, url : str, params : Dict[str, Any] | None = None) -> Dict[str, Any]:
+    resp = client.get(url, params=params)
+    assert resp.status_code == 200, f"Unexpected status code: {resp.status_code}. {resp.content}"
+    return resp.json()
+
+def test_changepoint_list(client):
+    resp = getj(client, "/api/v1/detector/chagepoints", params={"probe_cc":"VE", "probe_asn":"AS8048", "domain" : "google.com"})
