17e9976472fe57ed9ec2b39870db2bcb005f3834
Luis Díaz <ldiazn98@gmail.com>
2025-10-28 12:38:20 +0100

    Add basic filtering test to changepoint endpoint

--- ooniapi/services/oonimeasurements/tests/test_event_detection.py
+++ ooniapi/services/oonimeasurements/tests/test_event_detection.py
@@ -1,6 +1,7 @@
 from typing import Dict, Any
 import httpx
 from datetime import datetime
+import pytest
 
 
 def getj(
@@ -12,16 +13,60 @@ def getj(
     )
     return resp.json()
 
+# reasonable default since and until for the testing data
+since = datetime(2025, 10, 1)
+until = datetime(2025, 11, 1)
 
-def test_changepoint_list(client):
-    resp = getj(
+def getjsu(client, url, params={}):
+    # use this default since and until
+    # since the testing data has a fixed date that might
+    # become "obsolete" in the future for the default
+    # '30 days ago' since and until limits
+    params["since"] = since
+    params["until"] = until
+    return getj(client, url, params)
+
+def test_changepoint_list_basic(client):
+    resp = getjsu(
+        client,
+        "/api/v1/detector/chagepoints",
+    )
+
+    assert 'results' in resp, resp
+    assert len(resp['results']) > 0, resp['results']
+
+def normalize_asn(asn: int | str):
+    if isinstance(asn, int):
+        return asn
+    if isinstance(asn, str) and asn.upper().startswith('AS'):
+        asn = asn.upper().strip("AS")
+
+    return int(asn)
+
+@pytest.mark.parametrize(
+    "filter_param, filter_value",
+    [
+        ("probe_asn", 8048),
+        ("probe_asn", "AS8048"),# filter asn by string is also valid
+        ("probe_asn", "8048"),
+        ("probe_cc", "VE"),
+        ("domain", "google.com")
+    ],
+)
+def test_changepoint_filter_basic(client, filter_param, filter_value):
+
+    # Check filtering by cc
+    resp = getjsu(
         client,
         "/api/v1/detector/chagepoints",
         params={
-            "probe_cc": "VE",
-            "probe_asn": "AS8048",
-            "domain": "google.com",
-            "since" : datetime(2025, 10, 1),
-            "until" : datetime(2025, 11, 1)
-            },
+            filter_param : filter_value
+        }
     )
+
+    assert len(resp['results']) > 0, "No results to validate"
+
+    if filter_param == "probe_asn":
+        assert all(r[filter_param] == normalize_asn(filter_value) for r in resp['results']), resp['results']
+    else:
+        assert all(r[filter_param] == filter_value for r in resp['results']), resp['results']
