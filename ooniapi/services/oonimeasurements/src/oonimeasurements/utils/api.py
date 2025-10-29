3620c73859b6d067c93c6ae3ba86f56e99298400
Luis Díaz <ldiazn98@gmail.com>
2025-10-29 10:40:37 +0100

    Remove unused types

--- ooniapi/services/oonimeasurements/src/oonimeasurements/utils/api.py
+++ ooniapi/services/oonimeasurements/src/oonimeasurements/utils/api.py
@@ -7,19 +7,3 @@ from fastapi import Query
 
 ProbeCCOrNone = Annotated[Optional[str], Query(min_length=2, max_length=2)]
 ProbeASNOrNone = Annotated[Union[int, str, None], Query()]
-
-Limit = Annotated[
-        int,
-        Query(
-            description="Number of records to return (default: 100)",
-            ge=0, le=1_000_000, default=100
-        ),
-    ]
-
-Offset = Annotated[
-    int,
-    Query(
-        description="Offset from the start of the query, should be greater or equal to 0",
-        ge=0, default=0
-    )
-]
