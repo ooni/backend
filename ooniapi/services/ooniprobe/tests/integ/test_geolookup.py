ba41ff3d734440864184982c0a6c09f1a17c966f
Aaron Gibson <aagbsn@extc.org>
2025-11-07 14:11:04 +0000

    monkeypatch lookup_probe_cc and lookup_probe_network to provide static responses

--- ooniapi/services/ooniprobe/tests/integ/test_geolookup.py
+++ ooniapi/services/ooniprobe/tests/integ/test_geolookup.py
@@ -1,4 +1,18 @@
-def test_geolookup(client):
+from typing import Dict, Tuple
+import ooniprobe.routers.v1.probe_services as ps
+from ooniprobe.utils import lookup_probe_cc, lookup_probe_network
+from ooniprobe.dependencies import CCReaderDep, ASNReaderDep
+
+def fake_lookup_probe_network(ipaddr: str, asn_reader: ASNReaderDep) -> Tuple[str, str]:
+    return ("AS4242", "Testing Networks")
+
+def fake_lookup_probe_cc(ipaddr: str, cc_reader: CCReaderDep) -> str:
+    return "US"
+
+
+def test_geolookup(client, monkeypatch):
+    monkeypatch.setattr(ps, "lookup_probe_network", fake_lookup_probe_network)
+    monkeypatch.setattr(ps, "lookup_probe_cc", fake_lookup_probe_cc)
     j = dict(
         addresses=["192.33.4.12", "170.247.170.2", "2801:1b8:10::b", "2001:500:2::c"]
     )
@@ -6,7 +20,8 @@ def test_geolookup(client):
     assert "geolocation" in c
     assert "v" in c
     g = c["geolocation"]
+
     for ip in j["addresses"]:
-        assert g[ip]["cc"] != "ZZ"
-        assert g[ip]["asn"] != "AS0"
-        assert g[ip]["as_name"] is not None
+        assert g[ip]["cc"] == "US"
+        assert g[ip]["asn"] == "AS4242"
+        assert g[ip]["as_name"] == "Testing Networks"
