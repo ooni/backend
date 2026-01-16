f9bfd07c84d2cad940520873536ff5ae6cdc2375
Aaron Gibson <aagbsn@extc.org>
2026-01-16 17:22:41 +0000

    remove fix for not-a-bug

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -28,22 +28,10 @@ def app():
     return app
 
 
-@pytest.yield_fixture
+@pytest.fixture
 def client(app):
-    """
-    Overriding the `client` fixture from pytest_flask to fix this bug:
-    https://github.com/pytest-dev/pytest-flask/issues/42
-    """
     with app.test_client() as client:
-        yield client
-
-    while True:
-        top = flask._request_ctx_stack.top
-        if top is not None and top.preserved:
-            top.pop()
-        else:
-            break
-
+        return client
 
 @pytest.fixture(autouse=True)
 def disable_rate_limits(app):
