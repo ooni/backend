1cdaeaa8fe4eaecbbdcc688b804689f54ffd2758
Arturo Filastò <arturo@filasto.net>
2026-02-17 17:29:14 +0100

    Fix geolookup tests

--- ooniapi/services/ooniprobe/tests/integ/test_geolookup.py
+++ ooniapi/services/ooniprobe/tests/integ/test_geolookup.py
@@ -1,18 +1,24 @@
 from typing import Tuple
+
 import ooniprobe.routers.v1.probe_services as ps
-from ooniprobe.dependencies import CCReaderDep, ASNReaderDep
+from ooniprobe.dependencies import ASNReaderDep, CCReaderDep
+
 
 def fake_lookup_probe_network(ipaddr: str, asn_reader: ASNReaderDep) -> Tuple[str, str]:
     return ("AS4242", "Testing Networks")
 
+
 def fake_lookup_probe_cc(ipaddr: str, cc_reader: CCReaderDep) -> str:
     return "US"
+
 
 def missing_lookup_probe_network(ipaddr: str, asn_reader: ASNReaderDep):
     return ("AS0", None)
 
+
 def missing_lookup_probe_cc(ipaddr: str, cc_reader: CCReaderDep) -> str:
     return "ZZ"
+
 
 def test_geolookup(client, monkeypatch):
     monkeypatch.setattr(ps, "lookup_probe_network", fake_lookup_probe_network)
@@ -27,15 +33,14 @@ def test_geolookup(client, monkeypatch):
 
     for ip in j["addresses"]:
         assert g[ip]["cc"] == "US"
-        assert g[ip]["asn"] == "AS4242"
+        assert g[ip]["asn"] == 4242
         assert g[ip]["as_name"] == "Testing Networks"
+
 
 def test_missing_geolookup(client, monkeypatch):
     monkeypatch.setattr(ps, "lookup_probe_network", missing_lookup_probe_network)
     monkeypatch.setattr(ps, "lookup_probe_cc", missing_lookup_probe_cc)
-    j = dict(
-        addresses=["1.2.3.4", "127.0.0.1"]
-    )
+    j = dict(addresses=["1.2.3.4", "127.0.0.1"])
     c = client.post("/api/v1/geolookup", json=j).json()
     assert "geolocation" in c
     assert "v" in c
@@ -43,5 +48,5 @@ def test_missing_geolookup(client, monkeypatch):
 
     for ip in j["addresses"]:
         assert g[ip]["cc"] == "ZZ"
-        assert g[ip]["asn"] == "AS0"
+        assert g[ip]["asn"] == 0
         assert g[ip]["as_name"] == ""
