221d71ee7d7f984afa7574abf5f89735fcdf344e
Luis Díaz <ldiazn98@gmail.com>
2026-05-20 11:01:28 +0200

    Add check that passes http header field manipulation test names

--- ooniapi/services/ooniprobe/tests/test_utils.py
+++ ooniapi/services/ooniprobe/tests/test_utils.py
@@ -1,4 +1,11 @@
-from ooniprobe.utils import get_first_ip
+from ooniprobe.utils import check_measurement_meta, get_first_ip
+
+
+def test_check_measurement_meta_http_header_field_manipulation():
+    test_name = "http_header_field_manipulation"
+    assert len(test_name) == 30
+    check_measurement_meta(test_name, "US", "AS30722")
+
 
 def test_get_first_ip():
 
